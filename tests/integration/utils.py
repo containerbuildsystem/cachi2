cc422d9f7d0f9c21c863cf5fb7fcaa1b35bf7af2
Bruno Pimentel <bpimente@redhat.com>
2023-01-30 09:58:13 -0300

    Prefixes integration test sha256 checksums
    Since the previous commit added "gitcommit" as a type of checksum, this
    change is now prefixing the existing sha256 checksums to make clear what
    the data represents.
    
    Signed-off-by: Bruno Pimentel <bpimente@redhat.com>

--- tests/integration/utils.py
+++ tests/integration/utils.py
@@ -159,7 +159,7 @@ def calculate_sha256sum(file: str) -> str:
         # Read and update hash string value in blocks of 4K
         for byte_block in iter(lambda: f.read(4096), b""):
             sha256_hash.update(byte_block)
-    return sha256_hash.hexdigest()
+    return f"sha256:{sha256_hash.hexdigest()}"
 
 
 def load_json(file: str) -> Dict:
