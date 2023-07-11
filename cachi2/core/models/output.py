7412247b76eeb4b7ba4caa8ffb3ca0a251af4fc1
Bruno Pimentel <bpimente@redhat.com>
2023-07-11 15:25:27 -0300

    Include Cachi2's name in the SBOM metadata
    Cachi2 is included in the .metadata.tools section, and also in each
    component's properties.
    
    Signed-off-by: Bruno Pimentel <bpimente@redhat.com>

--- cachi2/core/models/output.py
+++ cachi2/core/models/output.py
@@ -10,6 +10,16 @@ from cachi2.core.models.validators import unique_sorted
 log = logging.getLogger(__name__)
 
 
+class Property(pydantic.BaseModel):
+    """A property inside an SBOM component."""
+
+    name: str
+    value: str
+
+
+FOUND_BY_CACHI2_PROPERTY: Property = Property(name="cachi2:found_by", value="cachi2")
+
+
 class Component(pydantic.BaseModel):
     """A software component such as a dependency or a package.
 
@@ -19,6 +29,7 @@ class Component(pydantic.BaseModel):
 
     name: str
     version: Optional[str]
+    properties: list[Property] = []
     purl: Optional[str]  # optional while it is not implemented for all package managers
     type: Literal["library"] = "library"
 
@@ -31,6 +42,13 @@ class Component(pydantic.BaseModel):
             return self.purl
 
         return f"{self.name}:{self.version}"
+
+    @pydantic.validator("properties", always=True)
+    def _add_found_by_property(cls, properties: list[Property]) -> list[Property]:
+        if FOUND_BY_CACHI2_PROPERTY not in properties:
+            properties.append(FOUND_BY_CACHI2_PROPERTY)
+
+        return properties
 
     @classmethod
     def from_package_dict(cls, package: dict[str, Any]) -> "Component":
@@ -45,6 +63,19 @@ class Component(pydantic.BaseModel):
         )
 
 
+class Tool(pydantic.BaseModel):
+    """A tool used to generate the SBOM content."""
+
+    vendor: str
+    name: str
+
+
+class Metadata(pydantic.BaseModel):
+    """Metadata field in a SBOM."""
+
+    tools: list[Tool] = [Tool(vendor="red hat", name="cachi2")]
+
+
 class Sbom(pydantic.BaseModel):
     """Software bill of materials in the CycloneDX format.
 
@@ -54,6 +85,7 @@ class Sbom(pydantic.BaseModel):
 
     bom_format: Literal["CycloneDX"] = pydantic.Field(alias="bomFormat", default="CycloneDX")
     components: list[Component] = []
+    metadata: Metadata = Metadata()
     spec_version: str = pydantic.Field(alias="specVersion", default="1.4")
     version: int = 1
 
