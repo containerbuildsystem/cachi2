e3d72ed15b6f17726e81b264d2c6150586e215bd
Ladislav Kolacek <lkolacek@redhat.com>
2023-07-17 14:55:59 +0200

    Update integration tests and test data
    * Updated package.json files with local paths to fetched packages were added to build-config.json
    * Promote npm test scenarios (npm_smoketest_lockfile2, npm_smoketest_lockfile3) to end to end tests
    * Add Containerfiles for new End to End tests
    * Change type of expected_cmd_output for end to end tests to list
    
    STONEBLD-1108
    
    Signed-off-by: Ladislav Kolacek <lkolacek@redhat.com>

--- tests/integration/test_npm.py
+++ tests/integration/test_npm.py
@@ -1,5 +1,6 @@
 import logging
 from pathlib import Path
+from typing import List
 
 import pytest
 
@@ -21,24 +22,6 @@ log = logging.getLogger(__name__)
                 check_vendor_checksums=False,
             ),
             id="npm_smoketest_lockfile1",
-        ),
-        pytest.param(
-            utils.TestParameters(
-                repo="https://github.com/cachito-testing/npm-cachi2-smoketest.git",
-                ref="b9a264fb7244f2cefa782feb1fd8c51ead9fb88b",
-                packages=({"path": ".", "type": "npm"},),
-                check_vendor_checksums=False,
-            ),
-            id="npm_smoketest_lockfile2",
-        ),
-        pytest.param(
-            utils.TestParameters(
-                repo="https://github.com/cachito-testing/npm-cachi2-smoketest.git",
-                ref="f229b5b9c9085dabf71622cc1204c5deef97fbe8",
-                packages=({"path": ".", "type": "npm"},),
-                check_vendor_checksums=False,
-            ),
-            id="npm_smoketest_lockfile3",
         ),
         # bundled dependencies, see the repo README for more details
         pytest.param(
@@ -100,4 +83,215 @@ def test_npm_smoketest(
 
     utils.fetch_deps_and_check_output(
         tmp_path, test_case, test_params, source_folder, test_data_dir, cachi2_image
+    )
+
+
+@pytest.mark.parametrize(
+    "test_params,check_cmd,expected_cmd_output",
+    [
+        pytest.param(
+            utils.TestParameters(
+                repo="https://github.com/cachito-testing/npm-cachi2-smoketest.git",
+                ref="532dd79bde494e90fae261afbb7b464dae2d2e32",
+                packages=({"path": ".", "type": "npm"},),
+                check_vendor_checksums=False,
+            ),
+            ["npm", "cache", "ls"],
+            [
+                "abbrev-2.0.0.tgz",
+                "accepts-1.3.8.tgz",
+                "array-flatten-1.1.1.tgz",
+                "bitbucket.org/cachi-testing/cachi2-without-deps-second/cachi2-without-deps-second-external-gitcommit-09992d418fc44a2895b7a9ff27c4e32d6f74a982.tgz",
+                "bitbucket.org/cachi-testing/cachi2-without-deps/cachi2-without-deps-external-gitcommit-9e164b97043a2d91bbeb992f6cc68a3d1015086a.tgz",
+                "body-parser-1.20.1.tgz",
+                "bytes-3.1.2.tgz",
+                "call-bind-1.0.2.tgz",
+                "classnames-2.3.2.tgz",
+                "colors-1.4.0.tgz",
+                "content-disposition-0.5.4.tgz",
+                "content-type-1.0.5.tgz",
+                "cookie-0.5.0.tgz",
+                "cookie-signature-1.0.6.tgz",
+                "csstype-3.1.2.tgz",
+                "dateformat-5.0.3.tgz",
+                "debug-2.6.9.tgz",
+                "depd-2.0.0.tgz",
+                "destroy-1.2.0.tgz",
+                "ee-first-1.1.1.tgz",
+                "encodeurl-1.0.2.tgz",
+                "escape-html-1.0.3.tgz",
+                "etag-1.8.1.tgz",
+                "express-4.18.2.tgz",
+                "external-cachito-npm-without-deps/cachito-npm-without-deps-external-sha512-43e71f90ad5f9eb349ab18a283f8954994def373962ddc61b866bdea4d48249e67913c6b84dca1e8c519e981ca1fcc62b438292104a88ee9ed72db76a41efede.tgz",
+                "fecha-4.2.3.tgz",
+                "finalhandler-1.2.0.tgz",
+                "forwarded-0.2.0.tgz",
+                "fresh-0.5.2.tgz",
+                "function-bind-1.1.1.tgz",
+                "get-intrinsic-1.2.0.tgz",
+                "github.com/kevva/is-positive/is-positive-external-gitcommit-97edff6f525f192a3f83cea1944765f769ae2678.tgz",
+                "has-1.0.3.tgz",
+                "has-symbols-1.0.3.tgz",
+                "http-errors-2.0.0.tgz",
+                "iconv-lite-0.4.24.tgz",
+                "inherits-2.0.4.tgz",
+                "ipaddr.js-1.9.1.tgz",
+                "media-typer-0.3.0.tgz",
+                "merge-descriptors-1.0.1.tgz",
+                "methods-1.1.2.tgz",
+                "mime-1.6.0.tgz",
+                "mime-db-1.52.0.tgz",
+                "mime-types-2.1.35.tgz",
+                "ms-2.0.0.tgz",
+                "ms-2.1.3.tgz",
+                "negotiator-0.6.3.tgz",
+                "object-inspect-1.12.3.tgz",
+                "on-finished-2.4.1.tgz",
+                "parseurl-1.3.3.tgz",
+                "path-to-regexp-0.1.7.tgz",
+                "proxy-addr-2.0.7.tgz",
+                "qs-6.11.0.tgz",
+                "range-parser-1.2.1.tgz",
+                "raw-body-2.5.1.tgz",
+                "safe-buffer-5.2.1.tgz",
+                "safer-buffer-2.1.2.tgz",
+                "sax-0.1.1.tgz",
+                "send-0.18.0.tgz",
+                "serve-static-1.15.0.tgz",
+                "setprototypeof-1.2.0.tgz",
+                "side-channel-1.0.4.tgz",
+                "statuses-2.0.1.tgz",
+                "toidentifier-1.0.1.tgz",
+                "type-is-1.6.18.tgz",
+                "types-prop-types-15.7.5.tgz",
+                "types-react-18.0.35.tgz",
+                "types-react-dom-18.0.11.tgz",
+                "types-scheduler-0.16.3.tgz",
+                "unpipe-1.0.0.tgz",
+                "utils-merge-1.0.1.tgz",
+                "uuid-9.0.0.tgz",
+                "vary-1.1.2.tgz",
+            ],
+            id="npm_smoketest_lockfile2",
+        ),
+        pytest.param(
+            utils.TestParameters(
+                repo="https://github.com/cachito-testing/npm-cachi2-smoketest.git",
+                ref="f1d31c2b051b218c84399b12461e0957d87bd0cd",
+                packages=({"path": ".", "type": "npm"},),
+                check_vendor_checksums=False,
+            ),
+            ["npm", "cache", "ls"],
+            [
+                "abbrev-2.0.0.tgz",
+                "accepts-1.3.8.tgz",
+                "array-flatten-1.1.1.tgz",
+                "bitbucket.org/cachi-testing/cachi2-without-deps-second/cachi2-without-deps-second-external-gitcommit-09992d418fc44a2895b7a9ff27c4e32d6f74a982.tgz",
+                "bitbucket.org/cachi-testing/cachi2-without-deps/cachi2-without-deps-external-gitcommit-9e164b97043a2d91bbeb992f6cc68a3d1015086a.tgz",
+                "body-parser-1.20.1.tgz",
+                "bytes-3.1.2.tgz",
+                "call-bind-1.0.2.tgz",
+                "classnames-2.3.2.tgz",
+                "colors-1.4.0.tgz",
+                "content-disposition-0.5.4.tgz",
+                "content-type-1.0.5.tgz",
+                "cookie-0.5.0.tgz",
+                "cookie-signature-1.0.6.tgz",
+                "csstype-3.1.2.tgz",
+                "dateformat-5.0.3.tgz",
+                "debug-2.6.9.tgz",
+                "depd-2.0.0.tgz",
+                "destroy-1.2.0.tgz",
+                "ee-first-1.1.1.tgz",
+                "encodeurl-1.0.2.tgz",
+                "escape-html-1.0.3.tgz",
+                "etag-1.8.1.tgz",
+                "express-4.18.2.tgz",
+                "external-cachito-npm-without-deps/cachito-npm-without-deps-external-sha512-43e71f90ad5f9eb349ab18a283f8954994def373962ddc61b866bdea4d48249e67913c6b84dca1e8c519e981ca1fcc62b438292104a88ee9ed72db76a41efede.tgz",
+                "fecha-4.2.3.tgz",
+                "finalhandler-1.2.0.tgz",
+                "forwarded-0.2.0.tgz",
+                "fresh-0.5.2.tgz",
+                "function-bind-1.1.1.tgz",
+                "get-intrinsic-1.2.0.tgz",
+                "github.com/kevva/is-positive/is-positive-external-gitcommit-97edff6f525f192a3f83cea1944765f769ae2678.tgz",
+                "has-1.0.3.tgz",
+                "has-symbols-1.0.3.tgz",
+                "http-errors-2.0.0.tgz",
+                "iconv-lite-0.4.24.tgz",
+                "inherits-2.0.4.tgz",
+                "ipaddr.js-1.9.1.tgz",
+                "media-typer-0.3.0.tgz",
+                "merge-descriptors-1.0.1.tgz",
+                "methods-1.1.2.tgz",
+                "mime-1.6.0.tgz",
+                "mime-db-1.52.0.tgz",
+                "mime-types-2.1.35.tgz",
+                "ms-2.0.0.tgz",
+                "ms-2.1.3.tgz",
+                "negotiator-0.6.3.tgz",
+                "object-inspect-1.12.3.tgz",
+                "on-finished-2.4.1.tgz",
+                "parseurl-1.3.3.tgz",
+                "path-to-regexp-0.1.7.tgz",
+                "proxy-addr-2.0.7.tgz",
+                "qs-6.11.0.tgz",
+                "range-parser-1.2.1.tgz",
+                "raw-body-2.5.1.tgz",
+                "safe-buffer-5.2.1.tgz",
+                "safer-buffer-2.1.2.tgz",
+                "sax-0.1.1.tgz",
+                "send-0.18.0.tgz",
+                "serve-static-1.15.0.tgz",
+                "setprototypeof-1.2.0.tgz",
+                "side-channel-1.0.4.tgz",
+                "statuses-2.0.1.tgz",
+                "toidentifier-1.0.1.tgz",
+                "type-is-1.6.18.tgz",
+                "types-prop-types-15.7.5.tgz",
+                "types-react-18.0.35.tgz",
+                "types-react-dom-18.0.11.tgz",
+                "types-scheduler-0.16.3.tgz",
+                "unpipe-1.0.0.tgz",
+                "utils-merge-1.0.1.tgz",
+                "uuid-9.0.0.tgz",
+                "vary-1.1.2.tgz",
+            ],
+            id="npm_smoketest_lockfile3",
+        ),
+    ],
+)
+def test_e2e_npm(
+    test_params: utils.TestParameters,
+    check_cmd: List[str],
+    expected_cmd_output: str,
+    cachi2_image: utils.ContainerImage,
+    tmp_path: Path,
+    test_data_dir: Path,
+    request: pytest.FixtureRequest,
+) -> None:
+    """
+    End to end test for npm.
+
+    :param test_params: Test case arguments
+    :param tmp_path: Temp directory for pytest
+    """
+    test_case = request.node.callspec.id
+
+    source_folder = utils.clone_repository(
+        test_params.repo, test_params.ref, f"{test_case}-source", tmp_path
+    )
+
+    output_folder = utils.fetch_deps_and_check_output(
+        tmp_path, test_case, test_params, source_folder, test_data_dir, cachi2_image
+    )
+
+    utils.build_image_and_check_cmd(
+        tmp_path,
+        output_folder,
+        test_data_dir,
+        test_case,
+        check_cmd,
+        expected_cmd_output,
+        cachi2_image,
     )
