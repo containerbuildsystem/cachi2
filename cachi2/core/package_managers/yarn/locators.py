454446316bde693a67876583fcd5e76f78c69772
Adam Cmiel <acmiel@redhat.com>
2023-09-27 13:03:50 +0200

    Clean up Locator classes
    STONEBLD-1749
    
    Improve example strings
    
    Remove methods that won't be useful
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- cachi2/core/package_managers/yarn/locators.py
+++ cachi2/core/package_managers/yarn/locators.py
@@ -13,94 +13,44 @@ class FileLocator(NamedTuple):
     """Locator that handles file, portal and link protocols.
 
     Sample locator string:
-    name@protocol:path-to-local-file-or-folder::locator=project-name%40workspace%3Apath
+        [@scope/]name@file:path/to/tarball.tar.gz::locator=<workspace-locator>
+        [@scope/]name@file:path/to/directory#path/to/directory::hash=321cba&locator=<workspace-locator>
+        [@scope/]name@link:path/to/directory::locator=<workspace-locator>
+        [@scope/]name@portal:path/to/directory::locator=<workspace-locator>
     """
-
-    @classmethod
-    def from_string(cls, locator: str) -> "FileLocator":
-        """Parse a Locator from a string."""
-        return NotImplemented
-
-    def __str__(self) -> str:
-        return NotImplemented
 
 
 class HttpsLocator(NamedTuple):
     """Locator that handles remote files fetched by HTTPS.
 
     Sample locator string:
-    name@https://domain.com/url/to/file.tar.gz
+        [@scope/]name@https://domain.com/url/to/file.tar.gz
     """
-
-    @classmethod
-    def from_string(cls, locator: str) -> "HttpsLocator":
-        """Parse a Locator from a string."""
-        return NotImplemented
-
-    def __str__(self) -> str:
-        return NotImplemented
 
 
 class NpmLocator(NamedTuple):
     """Locator that handles registry dependencies.
 
-    Sample locator strings:
-    name@npm:version
-    @scope/name@npm:version
+    Sample locator string:
+        [@scope/]name@npm:version
     """
-
-    @property
-    def is_scoped(self) -> bool:
-        """Check if this package is scoped.
-
-        A scoped package follows the naming format of @scope/name.
-        """
-        return NotImplemented
-
-    def scope(self) -> str:
-        """Return the scope of this package."""
-        return NotImplemented
-
-    @classmethod
-    def from_string(cls, locator: str) -> "NpmLocator":
-        """Parse a Locator from a string."""
-        return NotImplemented
-
-    def __str__(self) -> str:
-        return NotImplemented
 
 
 class PatchLocator(NamedTuple):
     """Locator that handles patched dependencies.
 
     Sample locator string:
-    name@patch:patched-dependency@npm%3A0.0.1#./path/to/patch::version=1.0.0&hash=abc123&locator=package-name%40workspace%3A
-    name@patch:patched-dependency@npm%3A0.0.1#built-in<compat/patch-name>::version=1.0.0&hash=abc123 (built-in patch)
+        [@scope/]name@patch:<patched-dep-locator>#path/to/patch::version=1.0.0&hash=abc123&locator=<workspace-locator>
+        [@scope/]name@patch:<patched-dep-locator>#~builtin<compat/patch-name>::version=1.0.0&hash=abc123
     """
-
-    @classmethod
-    def from_string(cls, locator: str) -> "PatchLocator":
-        """Parse a Locator from a string."""
-        return NotImplemented
-
-    def __str__(self) -> str:
-        return NotImplemented
 
 
 class WorkspaceLocator(NamedTuple):
     """Locator that handles workspace dependencies.
 
     Sample locator string:
-    name@workspace:path/to/dir
+        [@scope/]name@workspace:path/to/dir
     """
-
-    @classmethod
-    def from_string(cls, locator: str) -> "WorkspaceLocator":
-        """Parse a Locator from a string."""
-        return NotImplemented
-
-    def __str__(self) -> str:
-        return NotImplemented
 
 
 Locator = Union[
