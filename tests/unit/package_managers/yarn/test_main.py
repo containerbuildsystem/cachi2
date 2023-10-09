007d2569a226475ab9304904ece520ff8391e246
Taylor Madore <tmadore@redhat.com>
2023-10-09 09:25:57 -0400

    ensure packageManager is correctly set for yarn
    STONEBLD-1776
    
    Since we will rely on corepack to download and configure
    the correct version of yarn for a request, ensure that
    packageManager is defined in package.json.
    
    Use either yarnPath from .yarnrc, packageManager, or a
    combination of the two to set the correct yarn version.
    
    Raise exceptions for the following cases:
      - packageManager is already set, but we can't parse it
      - neither yarnPath or packageManager is set (or parseable)
      - the versions specified by yarnPath and packageManager
        are different
    
    Signed-off-by: Taylor Madore <tmadore@redhat.com>

--- tests/unit/package_managers/yarn/test_main.py
+++ tests/unit/package_managers/yarn/test_main.py
@@ -1,10 +1,101 @@
+import re
+from typing import Optional, Union
 from unittest import mock
 
 import pytest
+import semver
 
-from cachi2.core.errors import YarnCommandError
-from cachi2.core.package_managers.yarn.main import _fetch_dependencies
+from cachi2.core.errors import PackageRejected, UnexpectedFormat, YarnCommandError
+from cachi2.core.package_managers.yarn.main import _configure_yarn_version, _fetch_dependencies
 from cachi2.core.rooted_path import RootedPath
+
+
+@pytest.mark.parametrize(
+    "yarn_path_version, package_manager_version",
+    [
+        pytest.param(semver.VersionInfo(1, 0, 0), None, id="valid-yarnpath-no-packagemanager"),
+        pytest.param(None, semver.VersionInfo(1, 0, 0), id="no-yarnpath-valid-packagemanager"),
+        pytest.param(
+            semver.VersionInfo(1, 0, 0),
+            semver.VersionInfo(1, 0, 0),
+            id="matching-yarnpath-and-packagemanager",
+        ),
+        pytest.param(
+            semver.VersionInfo(1, 0, 0),
+            semver.VersionInfo(
+                1, 0, 0, build="sha224.953c8233f7a92884eee2de69a1b92d1f2ec1655e66d08071ba9a02fa"
+            ),
+            id="matching-yarnpath-and-packagemanager-with-build",
+        ),
+    ],
+)
+@mock.patch("cachi2.core.package_managers.yarn.main.get_semver_from_package_manager")
+@mock.patch("cachi2.core.package_managers.yarn.main.get_semver_from_yarn_path")
+def test_configure_yarn_version(
+    mock_yarn_path_semver: mock.Mock,
+    mock_package_manager_semver: mock.Mock,
+    yarn_path_version: Optional[semver.version.Version],
+    package_manager_version: Optional[semver.version.Version],
+) -> None:
+    mock_project = mock.Mock()
+    mock_project.package_json.package_manager = None
+    mock_yarn_path_semver.return_value = yarn_path_version
+    mock_package_manager_semver.return_value = package_manager_version
+
+    _configure_yarn_version(mock_project)
+
+    if package_manager_version is None:
+        assert mock_project.package_json.package_manager == f"yarn@{yarn_path_version}"
+        mock_project.package_json.write_to_file.assert_called_once()
+    else:
+        assert mock_project.package_json.package_manager is None
+        mock_project.package_json.write_to_file.assert_not_called()
+
+
+@pytest.mark.parametrize(
+    "yarn_path_version, package_manager_version, expected_error",
+    [
+        pytest.param(
+            None,
+            None,
+            PackageRejected(
+                "Unable to determine the yarn version to use to process the request",
+                solution="Ensure that either yarnPath is defined in .yarnrc or that packageManager is defined in package.json",
+            ),
+            id="no-yarnpath-no-packagemanager",
+        ),
+        pytest.param(
+            None,
+            UnexpectedFormat("some error about packageManager formatting"),
+            UnexpectedFormat("some error about packageManager formatting"),
+            id="exception-parsing-packagemanager",
+        ),
+        pytest.param(
+            semver.VersionInfo(1, 0, 1),
+            semver.VersionInfo(1, 0, 0),
+            PackageRejected(
+                "Mismatch between the yarn versions specified by yarnPath (yarn@1.0.1) and packageManager (yarn@1.0.0)",
+                solution="Ensure that the yarnPath version in .yarnrc and the packageManager version in package.json agree",
+            ),
+            id="yarnpath-packagemanager-mismatch",
+        ),
+    ],
+)
+@mock.patch("cachi2.core.package_managers.yarn.main.get_semver_from_package_manager")
+@mock.patch("cachi2.core.package_managers.yarn.main.get_semver_from_yarn_path")
+def test_configure_yarn_version_fail(
+    mock_yarn_path_semver: mock.Mock,
+    mock_package_manager_semver: mock.Mock,
+    yarn_path_version: Optional[semver.version.Version],
+    package_manager_version: Union[semver.version.Version, None, Exception],
+    expected_error: Exception,
+) -> None:
+    mock_project = mock.Mock()
+    mock_yarn_path_semver.return_value = yarn_path_version
+    mock_package_manager_semver.side_effect = [package_manager_version]
+
+    with pytest.raises(type(expected_error), match=re.escape(str(expected_error))):
+        _configure_yarn_version(mock_project)
 
 
 @mock.patch("cachi2.core.package_managers.yarn.main.run_yarn_cmd")
