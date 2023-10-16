51ae95cb8c6f426fed10566cceb7ccce2b0c75ee
Adam Cmiel <acmiel@redhat.com>
2023-10-16 15:05:38 +0200

    Yarnberry: set PATH for the yarn subprocesses
    By passing a dict of environment variables to run_yarn_cmd, we overwrite
    the entire environment for the subprocess, including PATH.
    
    When running in the locally built container image, this prevents cachi2
    from finding the 'yarn' executable:
    
    - we use subprocess.run
    - subprocess.run delegates to Popen
    - Popen, on POSIX, "uses ox.execvpe()-like behavior" [1]
    - Python's os.execvpe [2], seemingly contrary to the POSIX execvpe [3],
      uses the environment that you pass to it as the source of the PATH
      variable
    - when PATH isn't defined, execvpe falls back to the value of
      confstr(_CS_PATH) (try 'getconf PATH')
    - the fallback path seems to be '/usr/bin' on Fedora
    - in the container, yarn is installed in /usr/local/bin
    
    TL;DR: running yarn works if you have yarn installed in /usr/bin, but
    not otherwise.
    
    Leaving PATH unset could also have other unforeseen side effects, such
    as when yarn itself tries to execute a subprocess.
    
    Always pass PATH to the yarn subprocess.
    
    [1]: https://docs.python.org/3/library/subprocess.html#popen-constructor
    [2]: https://docs.python.org/3/library/os.html#os.execvpe
    [3]: https://man7.org/linux/man-pages/man3/exec.3.html
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- cachi2/core/package_managers/yarn/utils.py
+++ cachi2/core/package_managers/yarn/utils.py
@@ -1,19 +1,27 @@
+import os
 import subprocess  # nosec
+from typing import Optional
 
 from cachi2.core.errors import YarnCommandError
 from cachi2.core.rooted_path import RootedPath
 from cachi2.core.utils import run_cmd
 
 
-def run_yarn_cmd(cmd: list[str], source_dir: RootedPath, env: dict[str, str]) -> str:
+def run_yarn_cmd(
+    cmd: list[str], source_dir: RootedPath, env: Optional[dict[str, str]] = None
+) -> str:
     """Run a yarn command on a source directory.
 
     :param cmd: the command that will be executed, split in a list of strings in every space.
     :param source_dir: the directory in the repository containing the yarn source files.
-    :param env: environment variables to be set during the command's execution.
-
+    :param env: environment variables to be set during the command's execution
     :raises YarnCommandError: if the command fails.
     """
+    env = env or {}
+    # if the caller doesn't specify a PATH variable, then pass the PATH from the current
+    # process to the subprocess
+    if "PATH" not in env and (self_path := os.environ.get("PATH")):
+        env = env | {"PATH": self_path}
     try:
         return run_cmd(cmd=["yarn", *cmd], params={"cwd": source_dir, "env": env})
     except subprocess.CalledProcessError:
