d27377dcf2c32376005286f4f69c9cff51a101ab
Adam Cmiel <acmiel@redhat.com>
2023-10-20 13:06:01 +0200

    run_cmd: execute programs by their absolute path
    To avoid complex and platform-dependent interaction of subprocess.run
    with the 'env' parameter (see the previous commit), execute programs by
    abspath so that subprocess.run doesn't have to look for them in PATH.
    
    To get the abspath, use shutil.which(), which also looks for the program
    in PATH but doesn't have the gotchas that subprocess.run does.
    
    If the program is not found, raise a friendlier error than the default
    FileNotFoundError: [Errno 2] No such file or directory: 'foo'
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -4,10 +4,12 @@ from unittest import mock
 
 import pytest
 
+from cachi2.core.errors import Cachi2Error
 from cachi2.core.utils import run_cmd
 
 
 @mock.patch("subprocess.run")
+@mock.patch("shutil.which")
 @pytest.mark.parametrize(
     "stdout, stderr, expect_logs",
     [
@@ -26,12 +28,14 @@ from cachi2.core.utils import run_cmd
     ],
 )
 def test_run_cmd_logs_stdouterr_on_failure(
+    mock_shutil_which: mock.Mock,
     mock_subprocess_run: mock.Mock,
     stdout: Optional[str],
     stderr: Optional[str],
     expect_logs: list[str],
     caplog: pytest.LogCaptureFixture,
 ) -> None:
+    mock_shutil_which.return_value = "/usr/local/bin/some"
     mock_subprocess_run.return_value = subprocess.CompletedProcess(
         ["some", "command"], returncode=1, stdout=stdout, stderr=stderr
     )
@@ -40,3 +44,38 @@ def test_run_cmd_logs_stdouterr_on_failure(
         run_cmd(["some", "command"], {})
 
     assert caplog.messages == ['The command "some command" failed', *expect_logs]
+
+
+@mock.patch("subprocess.run")
+@mock.patch("shutil.which")
+@pytest.mark.parametrize("extra_args", [True, False])
+def test_run_cmd_execute_by_abspath(
+    mock_shutil_which: mock.Mock,
+    mock_subprocess_run: mock.Mock,
+    extra_args: bool,
+) -> None:
+    mock_shutil_which.return_value = "/usr/local/bin/yarn"
+    mock_subprocess_run.return_value = subprocess.CompletedProcess([], returncode=0)
+
+    if extra_args:
+        cmd = ["yarn", "--version"]
+        expect_cmd = ["/usr/local/bin/yarn", "--version"]
+    else:
+        cmd = ["yarn"]
+        expect_cmd = ["/usr/local/bin/yarn"]
+
+    run_cmd(cmd, params={})
+
+    mock_subprocess_run.assert_called_once()
+    call = mock_subprocess_run.call_args_list[0]
+    assert call.args[0] == expect_cmd
+
+
+@mock.patch("shutil.which")
+def test_run_cmd_executable_not_found(
+    mock_shutil_which: mock.Mock,
+) -> None:
+    mock_shutil_which.return_value = None
+
+    with pytest.raises(Cachi2Error, match="'foo' executable not found in PATH"):
+        run_cmd(["foo"], params={})
