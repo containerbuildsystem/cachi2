cf0bce9c37025c6f2f41b178c4760284b5c177d0
Erik Skultety <eskultet@redhat.com>
2023-11-13 14:48:11 +0100

    package_managers: yarn: Fail on requests for Yarn non-v3
    Initially we're only going to support Yarn Berry v3 since:
    - v1 is marked as legacy getting only maintenance releases, lacking
      functionality we require
    - v2 lacks the subcommand 'config' and '--mode=skip-build' flag
    - v4 is an unstable release at the moment
    
    STONEBLD-1891
    
    Signed-off-by: Erik Skultety <eskultet@redhat.com>

--- tests/unit/package_managers/yarn/test_main.py
+++ tests/unit/package_managers/yarn/test_main.py
@@ -1,5 +1,7 @@
 import re
-from typing import Optional, Union
+from enum import Enum
+from itertools import zip_longest
+from typing import List, Optional, Union
 from unittest import mock
 
 import pytest
@@ -16,14 +18,35 @@ from cachi2.core.package_managers.yarn.project import YarnRc
 from cachi2.core.rooted_path import RootedPath
 
 
+class YarnVersions(Enum):
+    YARN_V1 = semver.VersionInfo(1, 0, 0)
+    YARN_V2 = semver.VersionInfo(2, 0, 0)
+
+    YARN_V3_RC1 = semver.VersionInfo(3, 0, 0, prerelease="rc1")
+    YARN_V3 = semver.VersionInfo(3, 0, 0)
+    YARN_V36_RC1 = semver.VersionInfo(3, 6, 0, prerelease="rc1")
+
+    YARN_V4_RC1 = semver.VersionInfo(4, 0, 0, prerelease="rc1")
+    YARN_V4 = semver.VersionInfo(4, 0, 0)
+
+    @classmethod
+    def supported(cls) -> List["YarnVersions"]:
+        return [cls.YARN_V3, cls.YARN_V36_RC1]
+
+    @classmethod
+    def unsupported(cls) -> List["YarnVersions"]:
+        return list(set(cls.__members__.values()).difference(set(cls.supported())))
+
+
 @pytest.mark.parametrize(
     "yarn_path_version, package_manager_version",
     [
-        pytest.param(semver.VersionInfo(3, 0, 0), None, id="valid-yarnpath-no-packagemanager"),
-        pytest.param(None, semver.VersionInfo(3, 0, 0), id="no-yarnpath-valid-packagemanager"),
+        pytest.param(YarnVersions.YARN_V3.value, None, id="valid-yarnpath-no-packagemanager"),
+        pytest.param(YarnVersions.YARN_V36_RC1.value, None, id="minor-version-with-prerelease"),
+        pytest.param(None, YarnVersions.YARN_V3.value, id="no-yarnpath-valid-packagemanager"),
         pytest.param(
-            semver.VersionInfo(3, 0, 0),
-            semver.VersionInfo(3, 0, 0),
+            YarnVersions.YARN_V3.value,
+            YarnVersions.YARN_V3.value,
             id="matching-yarnpath-and-packagemanager",
         ),
         pytest.param(
@@ -101,6 +124,38 @@ def test_configure_yarn_version_fail(
     mock_package_manager_semver.side_effect = [package_manager_version]
 
     with pytest.raises(type(expected_error), match=re.escape(str(expected_error))):
+        _configure_yarn_version(mock_project)
+
+
+YARN_VERSIONS = [yarn_version.value for yarn_version in YarnVersions.unsupported()]
+
+
+@pytest.mark.parametrize(
+    "package_manager_version, yarn_path_version",
+    [
+        pytest.param(
+            pkg_mgr_version,
+            yarn_path_version,
+            id=f"package_manager,yarn_path-({str(pkg_mgr_version)}, {str(yarn_path_version)})",
+        )
+        for pkg_mgr_version, yarn_path_version in zip_longest(YARN_VERSIONS, YARN_VERSIONS[:1])
+    ],
+)
+@mock.patch("cachi2.core.package_managers.yarn.main.get_semver_from_package_manager")
+@mock.patch("cachi2.core.package_managers.yarn.main.get_semver_from_yarn_path")
+def test_yarn_unsupported_version_fail(
+    mock_yarn_path_semver: mock.Mock,
+    mock_package_manager_semver: mock.Mock,
+    package_manager_version: Union[semver.version.Version, None, Exception],
+    yarn_path_version: semver.version.Version,
+) -> None:
+    mock_project = mock.Mock()
+    mock_yarn_path_semver.return_value = None
+    mock_package_manager_semver.return_value = package_manager_version
+
+    with pytest.raises(
+        PackageRejected, match=f"Unsupported Yarn version '{package_manager_version}'"
+    ):
         _configure_yarn_version(mock_project)
 
 
