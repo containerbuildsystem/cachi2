23230aad18490abd6f0d5f5601a69a657babcd96
Erik Skultety <eskultet@redhat.com>
2023-11-20 14:53:10 +0100

    package_managers: yarn: Simplify _fetch_dependencies helper
    Now that we override Yarn's global cache folder using YarnRC file, we
    can simplify the _fetch_dependencies helper in that it doesn't need to
    supply the YARN_GLOBAL_FOLDER environment variable nor it needs to
    compute the resulting output directory for dependencies.
    
    Signed-off-by: Erik Skultety <eskultet@redhat.com>

--- tests/unit/package_managers/yarn/test_main.py
+++ tests/unit/package_managers/yarn/test_main.py
@@ -162,17 +162,15 @@ def test_yarn_unsupported_version_fail(
 @mock.patch("cachi2.core.package_managers.yarn.main.run_yarn_cmd")
 def test_fetch_dependencies(mock_yarn_cmd: mock.Mock, rooted_tmp_path: RootedPath) -> None:
     source_dir = rooted_tmp_path
-    output_dir = rooted_tmp_path.join_within_root("cachi2-output")
 
     mock_yarn_cmd.side_effect = YarnCommandError("berryscary")
 
     with pytest.raises(YarnCommandError) as exc_info:
-        _fetch_dependencies(source_dir, output_dir)
+        _fetch_dependencies(source_dir)
 
     mock_yarn_cmd.assert_called_once_with(
         ["install", "--mode", "skip-build"],
         source_dir,
-        {"YARN_GLOBAL_FOLDER": str(output_dir.join_within_root("deps", "yarn"))},
     )
 
     assert str(exc_info.value) == "berryscary"
