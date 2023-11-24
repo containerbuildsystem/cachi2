containerbuildsystem/cachi2

commit 5cd92cf84f7d14cbee0619b64167e69cb53b4095

Yarn: set the necessary configuration for processing the request
STONEBLD-1777

Set the following options in the .yarnrc.yml file:

- checksumBehavior: set to 'throw', so an exception is raised in case of
  checksum mismatches.
- enableImmutableInstalls: makes 'yarn install' fail in case a change to
  the lockfile is necessary.
- plugins: set to an empty array, so that no plugins will be used in the
  prefetch/cache checking. This avoids arbitrary code execution.
- pnpMode: set to 'strict', to disallow modules to require packages they
  don't explicitly list in their own dependencies.

For zero-installs:
- enableImmutableCache: makes 'yarn install' fail if any modification in
  the cache folder would be made.

For regular workflow:
- enableMirror: set to true to ensure the globalFolder will be used.
- globalFolder: set to the request's output folder. The prefetched
  packages will be placed in it.

More details at: https://v3.yarnpkg.com/configuration/yarnrc

Signed-off-by: Bruno Pimentel <bpimente@redhat.com>

cachi2/core/package_managers/yarn/main.py
@@ -22,6 +22,7 @@ def fetch_yarn_source(request: Request) -> RequestOutput:
     for package in request.yarn_packages:
         path = request.source_dir.join_within_root(package.path)
         project = Project.from_source_dir(path)
+
         components.extend(_resolve_yarn_project(project, request.output_dir))
 
     return RequestOutput.from_obj_list(
@@ -101,8 +102,20 @@ def _set_yarnrc_configuration(project: Project, output_dir: RootedPath) -> None:
     :param output_dir: in case the dependencies need to be fetched, this is where they will be
         downloaded to.
     """
-    # the plugins should be disabled here regardless of the project workflow.
-    pass
+    yarn_rc = project.yarn_rc
+
+    yarn_rc.plugins = []
+    yarn_rc.checksum_behavior = "throw"
+    yarn_rc.enable_immutable_installs = True
+    yarn_rc.pnp_mode = "strict"
+
+    if project.is_zero_installs:
+        yarn_rc.enable_immutable_cache = True
+    else:
+        yarn_rc.enable_mirror = True
+        yarn_rc.global_folder = str(output_dir)
+
+    yarn_rc.write()
 
 
 def _check_yarn_cache(source_dir: RootedPath) -> None:

tests/unit/package_managers/yarn/test_main.py
@@ -6,7 +6,12 @@ import pytest
 import semver
 
 from cachi2.core.errors import PackageRejected, UnexpectedFormat, YarnCommandError
-from cachi2.core.package_managers.yarn.main import _configure_yarn_version, _fetch_dependencies
+from cachi2.core.package_managers.yarn.main import (
+    _configure_yarn_version,
+    _fetch_dependencies,
+    _set_yarnrc_configuration,
+)
+from cachi2.core.package_managers.yarn.project import YarnRc
 from cachi2.core.rooted_path import RootedPath
 
 
@@ -115,3 +120,39 @@ def test_fetch_dependencies(mock_yarn_cmd: mock.Mock, rooted_tmp_path: RootedPat
     )
 
     assert str(exc_info.value) == "berryscary"
+
+
+@pytest.mark.parametrize(
+    "is_zero_installs",
+    (
+        pytest.param(True, id="zero-installs-project"),
+        pytest.param(False, id="regular-workflow-project"),
+    ),
+)
+@mock.patch("cachi2.core.package_managers.yarn.project.YarnRc.write")
+def test_set_yarnrc_configuration(mock_write: mock.Mock, is_zero_installs: bool) -> None:
+    yarn_rc = YarnRc(RootedPath("/tmp/.yarnrc.yml"), {})
+
+    project = mock.Mock()
+    project.is_zero_installs = is_zero_installs
+    project.yarn_rc = yarn_rc
+
+    output_dir = RootedPath("/tmp/output")
+
+    _set_yarnrc_configuration(project, output_dir)
+
+    expected_data = {
+        "checksumBehavior": "throw",
+        "enableImmutableInstalls": True,
+        "pnpMode": "strict",
+        "plugins": [],
+    }
+
+    if project.is_zero_installs:
+        expected_data["enableImmutableCache"] = True
+    else:
+        expected_data["enableMirror"] = True
+        expected_data["globalFolder"] = "/tmp/output"
+
+    assert yarn_rc._data == expected_data
+    assert mock_write.called_once()

cachi2/core/package_managers/yarn/project.py
@@ -8,7 +8,7 @@ import json
 import logging
 import re
 from pathlib import Path
-from typing import Any, NamedTuple, Optional
+from typing import Any, Literal, NamedTuple, Optional, TypedDict
 
 import semver
 import yaml
@@ -21,6 +21,16 @@ log = logging.getLogger(__name__)
 
 DEFAULT_CACHE_FOLDER = "./.yarn/cache"
 DEFAULT_REGISTRY = "https://registry.yarnpkg.com"
+
+ChecksumBehavior = Literal["throw", "update", "ignore"]
+PnpMode = Literal["strict", "loose"]
+
+
+class Plugin(TypedDict):
+    """A plugin defined in the yarnrc file."""
+
+    path: str
+    spec: str
 
 
 class YarnRc:
@@ -46,6 +56,72 @@ class YarnRc:
         Fallback to the default path in case the configuration key is missing.
         """
         return self._data.get("cacheFolder", DEFAULT_CACHE_FOLDER)
+
+    @property
+    def checksum_behavior(self) -> Optional[ChecksumBehavior]:
+        """Get the checksumBehavior configuration."""
+        return self._data.get("checksumBehavior", None)
+
+    @checksum_behavior.setter
+    def checksum_behavior(self, checksum_behavior: Optional[ChecksumBehavior]) -> None:
+        self._data["checksumBehavior"] = checksum_behavior
+
+    @property
+    def enable_immutable_cache(self) -> Optional[bool]:
+        """Get the enableImmutableCache configuration."""
+        return self._data.get("enableImmutableCache", None)
+
+    @enable_immutable_cache.setter
+    def enable_immutable_cache(self, enable_immutable_cache: Optional[bool]) -> None:
+        self._data["enableImmutableCache"] = enable_immutable_cache
+
+    @property
+    def enable_immutable_installs(self) -> Optional[bool]:
+        """Get the enableImmutableInstalls configuration."""
+        return self._data.get("enableImmutableInstalls", None)
+
+    @enable_immutable_installs.setter
+    def enable_immutable_installs(self, enable_immutable_installs: Optional[bool]) -> None:
+        self._data["enableImmutableInstalls"] = enable_immutable_installs
+
+    @property
+    def enable_mirror(self) -> Optional[bool]:
+        """Get the enableMirror configuration."""
+        return self._data.get("enableMirror", None)
+
+    @enable_mirror.setter
+    def enable_mirror(self, enable_mirror: Optional[bool]) -> None:
+        self._data["enableMirror"] = enable_mirror
+
+    @property
+    def global_folder(self) -> Optional[str]:
+        """Get the global folder."""
+        return self._data.get("globalFolder", None)
+
+    @global_folder.setter
+    def global_folder(self, global_folder: Optional[str]) -> None:
+        self._data["globalFolder"] = global_folder
+
+    @property
+    def pnp_mode(self) -> Optional[PnpMode]:
+        """Get the pnpMode configuration."""
+        return self._data.get("pnpMode", None)
+
+    @pnp_mode.setter
+    def pnp_mode(self, mode: Optional[PnpMode]) -> None:
+        self._data["pnpMode"] = mode
+
+    @property
+    def plugins(self) -> list[Plugin]:
+        """Get the configured plugins.
+
+        Returns an empty array in case there are none defined.
+        """
+        return self._data.get("plugins", [])
+
+    @plugins.setter
+    def plugins(self, plugins: list[Plugin]) -> None:
+        self._data["plugins"] = plugins
 
     @property
     def registry_server(self) -> str:

tests/unit/package_managers/yarn/test_project.py
@@ -15,28 +15,30 @@ from cachi2.core.package_managers.yarn.project import (
 )
 from cachi2.core.rooted_path import PathOutsideRoot, RootedPath
 
+# the supportedArchitectures property is not currently mapped in the YarnRc class, but is kept
+# here because it's one of the default properties generated by "yarn init -2"
 VALID_YARNRC_FILE = """
+cacheFolder: ./.custom/cache
+checksumBehavior: ignore
+enableImmutableCache: true
+enableImmutableInstalls: true
+enableInlineBuilds: true
+enableMirror: false
+globalFolder: /a/global/folder
+npmRegistryServer: https://registry.alternative.com
+npmScopes:
+    foobar:
+        npmRegistryServer: https://registry.foobar.com
+pnpMode: loose
 plugins:
   - path: .yarn/plugins/@yarnpkg/plugin-typescript.cjs
     spec: "@yarnpkg/plugin-typescript"
   - path: .yarn/plugins/@yarnpkg/plugin-exec.cjs
     spec: "@yarnpkg/plugin-exec"
-
-yarnPath: .custom/path/yarn-3.6.1.cjs
-
-enableInlineBuilds: true
-
 supportedArchitectures:
   os:
     - "linux"
-
-cacheFolder: ./.custom/cache
-
-npmRegistryServer: https://registry.alternative.com
-
-npmScopes:
-    foobar:
-        npmRegistryServer: https://registry.foobar.com
+yarnPath: .custom/path/yarn-3.6.1.cjs
 """
 
 EMPTY_YML_FILE = ""
@@ -71,6 +73,12 @@ def test_parse_yarnrc(rooted_tmp_path: RootedPath) -> None:
     yarn_rc = _prepare_yarnrc_file(rooted_tmp_path, VALID_YARNRC_FILE)
 
     assert yarn_rc.cache_folder == "./.custom/cache"
+    assert yarn_rc.checksum_behavior == "ignore"
+    assert yarn_rc.enable_immutable_cache is True
+    assert yarn_rc.enable_immutable_installs is True
+    assert yarn_rc.enable_mirror is False
+    assert yarn_rc.global_folder == "/a/global/folder"
+    assert yarn_rc.pnp_mode == "loose"
     assert yarn_rc.registry_server == "https://registry.alternative.com"
     assert yarn_rc.registry_server_for_scope("foobar") == "https://registry.foobar.com"
     assert yarn_rc.registry_server_for_scope("barfoo") == "https://registry.alternative.com"
@@ -81,6 +89,12 @@ def test_parse_empty_yarnrc(rooted_tmp_path: RootedPath) -> None:
     yarn_rc = _prepare_yarnrc_file(rooted_tmp_path, EMPTY_YML_FILE)
 
     assert yarn_rc.cache_folder == "./.yarn/cache"
+    assert yarn_rc.checksum_behavior is None
+    assert yarn_rc.enable_immutable_cache is None
+    assert yarn_rc.enable_immutable_installs is None
+    assert yarn_rc.enable_mirror is None
+    assert yarn_rc.global_folder is None
+    assert yarn_rc.pnp_mode is None
     assert yarn_rc.registry_server == "https://registry.yarnpkg.com"
     assert yarn_rc.registry_server_for_scope("foobar") == "https://registry.yarnpkg.com"
     assert yarn_rc.yarn_path is None

commit d0aa48c02293bdd35322b6054cc6e7cb76088364

package_managers: yarn: RC: Set the global cache folder correctly
We later override the global folder in _fetch_dependencies as
    YARN_GLOBAL_FOLDER=f"{output_dir.join_within_root('deps', 'yarn')"

which means that our initial default for the YarnRC's globalFolder
setting isn't correct.

Fixes: 5cd92cf84f7d14cbee0619b64167e69cb53b4095

Signed-off-by: Erik Skultety <eskultet@redhat.com>

cachi2/core/package_managers/yarn/main.py
@@ -127,7 +127,7 @@ def _set_yarnrc_configuration(project: Project, output_dir: RootedPath) -> None:
     yarn_rc.unsafe_http_whitelist = []
     yarn_rc.enable_mirror = True
     yarn_rc.enable_scripts = False
-    yarn_rc.global_folder = str(output_dir)
+    yarn_rc.global_folder = str(output_dir.join_within_root("deps", "yarn"))
 
     yarn_rc.write()
 

tests/unit/package_managers/yarn/test_main.py
@@ -212,7 +212,7 @@ def test_set_yarnrc_configuration(mock_write: mock.Mock) -> None:
         "enableScripts": False,
         "enableStrictSsl": True,
         "enableTelemetry": False,
-        "globalFolder": "/tmp/output",
+        "globalFolder": "/tmp/output/deps/yarn",
         "ignorePath": True,
         "unsafeHttpWhitelist": [],
         "pnpMode": "strict",