containerbuildsystem/cachi2

commit 2937416d596fda28cf3b0724239061885fe4e6d2

Add util function for recursively copying a directory
STONEBLD-1847

The intended use is to execute Cachi2's operations on a copy of the
source code, avoiding unwanted modifications to the original sources.

The function makes use of reflinks if they are supported by the file
system, as a means of speeding up the copy process.

The reflink library does not provide typing stubs, so ignore typing
was required.

Signed-off-by: Bruno Pimentel <bpimente@redhat.com>

cachi2/core/utils.py
@@ -3,7 +3,10 @@ import logging
 import re
 import shutil
 import subprocess  # nosec
-from typing import Iterator, Optional, Sequence
+from pathlib import Path
+from typing import Callable, Iterator, Optional, Sequence
+
+import reflink  # type: ignore
 
 from cachi2.core.config import get_config
 from cachi2.core.errors import Cachi2Error
@@ -74,3 +77,32 @@ def load_json_stream(s: str) -> Iterator:
     while match := non_whitespace.search(s, i):
         obj, i = decoder.raw_decode(s, match.start())
         yield obj
+
+
+def copy_directory(origin: Path, destination: Path) -> Path:
+    """
+    Recursively copy directory to another path.
+
+    Use reflinks by default if the file system supports it.
+
+    :raise FileExistsError: if the destination path already exists.
+    :raise FileNotFoundError: if the origin directory does not exist.
+    """
+
+    def _copy_using(copy_function: Callable) -> None:
+        shutil.copytree(
+            origin, destination, copy_function=copy_function, dirs_exist_ok=True, symlinks=True
+        )
+
+    if reflink.supported_at(origin):
+        try:
+            log.debug(f"Copying {origin} to {destination} using reflinks.")
+            _copy_using(reflink.reflink)
+        except reflink.ReflinkImpossibleError:
+            log.debug("Reflink copy failed, falling back to standard copy.")
+            _copy_using(shutil.copy2)
+    else:
+        log.debug(f"Copying {origin} to {destination} using a standard copy.")
+        _copy_using(shutil.copy2)
+
+    return destination

tests/unit/test_utils.py
@@ -1,11 +1,14 @@
+import shutil
 import subprocess
-from typing import Optional
+from pathlib import Path
+from typing import Callable, Optional
 from unittest import mock
 
 import pytest
+import reflink  # type: ignore
 
 from cachi2.core.errors import Cachi2Error
-from cachi2.core.utils import run_cmd
+from cachi2.core.utils import copy_directory, run_cmd
 
 
 @mock.patch("subprocess.run")
@@ -79,3 +82,73 @@ def test_run_cmd_executable_not_found(
 
     with pytest.raises(Cachi2Error, match="'foo' executable not found in PATH"):
         run_cmd(["foo"], params={})
+
+
+@mock.patch("shutil.copytree")
+@mock.patch("reflink.supported_at")
+@pytest.mark.parametrize(
+    "reflink_supported",
+    (True, False),
+)
+def test_copy_directory(
+    mock_reflink_supported_at: mock.Mock, mock_shutil_copytree: mock.Mock, reflink_supported: bool
+) -> None:
+    mock_reflink_supported_at.return_value = reflink_supported
+
+    origin = Path("/fake")
+    destination = Path("/phony")
+
+    copy_directory(origin, destination)
+
+    if reflink_supported:
+        copy_function = reflink.reflink
+    else:
+        copy_function = shutil.copy2
+
+    mock_shutil_copytree.assert_called_with(
+        origin, destination, copy_function=copy_function, dirs_exist_ok=True, symlinks=True
+    )
+
+
+@mock.patch("shutil.copytree")
+@mock.patch("reflink.supported_at")
+def test_copy_directory_with_reflink_failure(
+    mock_reflink_supported_at: mock.Mock,
+    mock_shutil_copytree: mock.Mock,
+) -> None:
+    def raise_reflink_error(
+        origin: Path,
+        destination: Path,
+        copy_function: Callable,
+        dirs_exist_ok: bool,
+        symlinks: bool,
+    ) -> None:
+        raise reflink.ReflinkImpossibleError()
+
+    mock_reflink_supported_at.return_value = True
+    mock_shutil_copytree.side_effect = raise_reflink_error
+
+    origin = Path("/fake")
+    destination = Path("/phony")
+
+    with pytest.raises(reflink.ReflinkImpossibleError):
+        copy_directory(origin, destination)
+
+    mock_shutil_copytree.assert_has_calls(
+        [
+            mock.call(
+                origin,
+                destination,
+                copy_function=reflink.reflink,
+                dirs_exist_ok=True,
+                symlinks=True,
+            ),
+            mock.call(
+                origin,
+                destination,
+                copy_function=shutil.copy2,
+                dirs_exist_ok=True,
+                symlinks=True,
+            ),
+        ]
+    )

Containerfile
@@ -5,11 +5,13 @@ WORKDIR /src
 RUN microdnf -y install \
     --setopt install_weak_deps=0 \
     --nodocs \
+    gcc \
     golang \
     git-core \
     nodejs \
     nodejs-npm \
     python3 \
+    python3-devel \
     python3-pip \
     && microdnf clean all
 

pyproject.toml
@@ -30,6 +30,7 @@ dependencies = [
   "pydantic",
   "pypi-simple",
   "pyyaml",
+  "reflink",
   "requests",
   "semver",
   "setuptools",

commit d7d6fb281cbfb10156840d1000c7da811a0834a1

core: utils: _copy_using: Put the dest dir name into the ignore pattern
Our default copy destination for the working copies of input
repositories is '.' which means that even the destination directory
(a temporary one) is copied recursively - luckily, the copy backend
seems to be smart enough not to get stuck in an infinite recursion.

Nevertheless, fix the issue by putting the destination dir name into
the ignore pattern of 'shutil.copytree'. This is fine because the
destination name is always going to be unique in our case and hence
there is close to 0% chance of a name collision preventing copying of
some relevant data (in case we don't copy to '.' by default anymore).

Fixes: 2937416d596fda28cf3b0724239061885fe4e6d2

Signed-off-by: Erik Skultety <eskultet@redhat.com>

cachi2/core/utils.py
@@ -91,7 +91,12 @@ def copy_directory(origin: Path, destination: Path) -> Path:
 
     def _copy_using(copy_function: Callable) -> None:
         shutil.copytree(
-            origin, destination, copy_function=copy_function, dirs_exist_ok=True, symlinks=True
+            origin,
+            destination,
+            copy_function=copy_function,
+            dirs_exist_ok=True,
+            symlinks=True,
+            ignore=shutil.ignore_patterns(destination.name),
         )
 
     if reflink.supported_at(origin):

tests/unit/test_utils.py
@@ -105,9 +105,7 @@ def test_copy_directory(
     else:
         copy_function = shutil.copy2
 
-    mock_shutil_copytree.assert_called_with(
-        origin, destination, copy_function=copy_function, dirs_exist_ok=True, symlinks=True
-    )
+    assert mock_shutil_copytree.call_args.kwargs["copy_function"] == copy_function
 
 
 @mock.patch("shutil.copy2")