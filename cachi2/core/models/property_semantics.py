e9e4b621a66b9915cf1975a5509b0b39a66e0695
Ben Alkov <ben.alkov@redhat.com>
2024-03-15 16:37:19 -0400

    feat(pkg-mgr:pip): add `pip_package_binary` property...
    ...to PropertySet and sbom::PropertyName so it can be processed
    
    Signed-off-by: Ben Alkov <ben.alkov@redhat.com>

--- cachi2/core/models/property_semantics.py
+++ cachi2/core/models/property_semantics.py
@@ -32,6 +32,7 @@ class PropertySet:
     missing_hash_in_file: frozenset[str] = field(default_factory=frozenset)
     npm_bundled: bool = False
     npm_development: bool = False
+    pip_package_binary: bool = False
 
     @classmethod
     def from_properties(cls, props: Iterable[Property]) -> "Self":
@@ -40,6 +41,7 @@ class PropertySet:
         missing_hash_in_file = []
         npm_bundled = False
         npm_development = False
+        pip_package_binary = False
 
         for prop in props:
             if prop.name == "cachi2:found_by":
@@ -50,10 +52,18 @@ class PropertySet:
                 npm_bundled = True
             elif prop.name == "cdx:npm:package:development":
                 npm_development = True
+            elif prop.name == "cachi2:pip:package:binary":
+                pip_package_binary = True
             else:
                 assert_never(prop.name)
 
-        return cls(found_by, frozenset(missing_hash_in_file), npm_bundled, npm_development)
+        return cls(
+            found_by,
+            frozenset(missing_hash_in_file),
+            npm_bundled,
+            npm_development,
+            pip_package_binary,
+        )
 
     def to_properties(self) -> list[Property]:
         """Convert a PropertySet to a list of SBOM component properties."""
@@ -68,6 +78,9 @@ class PropertySet:
             props.append(Property(name="cdx:npm:package:bundled", value="true"))
         if self.npm_development:
             props.append(Property(name="cdx:npm:package:development", value="true"))
+        if self.pip_package_binary:
+            props.append(Property(name="cachi2:pip:package:binary", value="true"))
+
         return sorted(props, key=lambda p: (p.name, p.value))
 
     def merge(self, other: "Self") -> "Self":
@@ -78,4 +91,5 @@ class PropertySet:
             missing_hash_in_file=self.missing_hash_in_file | other.missing_hash_in_file,
             npm_bundled=self.npm_bundled and other.npm_bundled,
             npm_development=self.npm_development and other.npm_development,
+            pip_package_binary=self.pip_package_binary or other.pip_package_binary,
         )
