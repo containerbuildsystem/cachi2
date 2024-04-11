a50a4e9f4cde89eb82ece771d6e3dfc2783fc9c9
Ondřej Nosek <onosek@redhat.com>
2024-04-11 01:51:35 +0000

    rpm: Introduce fetch_rpm_source
    Add `fetch-deps` functionality for the rpm package manager.
    RedHat lockfile format is validated and processed.
    
    Signed-off-by: Ondřej Nosek <onosek@redhat.com>

--- cachi2/core/package_managers/rpm/main.py
+++ cachi2/core/package_managers/rpm/main.py
@@ -1,20 +1,80 @@
 import logging
 
+import yaml
+from pydantic import ValidationError
+
+from cachi2.core.errors import PackageManagerError, PackageRejected
 from cachi2.core.models.input import Request
 from cachi2.core.models.output import RequestOutput
 from cachi2.core.models.sbom import Component
+from cachi2.core.package_managers.rpm.redhat import RedhatRpmsLock
+from cachi2.core.rooted_path import RootedPath
 
 log = logging.getLogger(__name__)
+
+
+DEFAULT_LOCKFILE_NAME = "rpms.lock.yaml"
+DEFAULT_PACKAGE_DIR = "deps/rpm"
+
+# during the computing of file checksum read chunk of size 1 MB
+READ_CHUNK = 1048576
 
 
 def fetch_rpm_source(request: Request) -> RequestOutput:
     """Process all the rpm source directories in a request."""
     components: list[Component] = []
     for package in request.rpm_packages:
-        _ = request.source_dir.join_within_root(package.path)
+        path = request.source_dir.join_within_root(package.path)
+        components.extend(_resolve_rpm_project(path, request.output_dir))
 
     return RequestOutput.from_obj_list(
         components=components,
         environment_variables=[],
         project_files=[],
     )
+
+
+def _resolve_rpm_project(source_dir: RootedPath, output_dir: RootedPath) -> list[Component]:
+    """
+    Process a request for a single RPM source directory.
+
+    Process the input lockfile, fetch packages and generate SBOM.
+    """
+    # Check the availability of the input lockfile.
+    if not source_dir.join_within_root(DEFAULT_LOCKFILE_NAME).path.exists():
+        raise PackageRejected(
+            f"RPM lockfile '{DEFAULT_LOCKFILE_NAME}' missing, refusing to continue.",
+            solution=(
+                "Make sure your repository has RPM lockfile '{DEFAULT_LOCKFILE_NAME}' checked in "
+                "to the repository."
+            ),
+        )
+
+    lockfile_name = source_dir.join_within_root(DEFAULT_LOCKFILE_NAME)
+    log.info(f"Reading RPM lockfile: {lockfile_name}")
+    with open(lockfile_name) as f:
+        try:
+            yaml_content = yaml.safe_load(f)
+        except yaml.YAMLError as e:
+            log.error(str(e))
+            raise PackageRejected(
+                f"RPM lockfile '{DEFAULT_LOCKFILE_NAME}' yaml format is not correct.",
+                solution=("Check correct 'yaml' syntax in the lockfile."),
+            )
+
+        log.debug("Validating lockfile.")
+        try:
+            _ = RedhatRpmsLock.model_validate(yaml_content)
+        except ValidationError as e:
+            loc = e.errors()[0]["loc"]
+            msg = e.errors()[0]["msg"]
+            raise PackageManagerError(
+                f"RPM lockfile '{DEFAULT_LOCKFILE_NAME}' format is not valid: '{loc}: {msg}'",
+                solution=(
+                    "Check the correct format and whether any keys are missing in the lockfile."
+                ),
+            )
+
+        _ = output_dir.join_within_root(DEFAULT_PACKAGE_DIR)
+        components: list[Component] = []
+        return components
--- tests/unit/package_managers/test_rpm.py
+++ tests/unit/package_managers/test_rpm.py
@@ -1,8 +1,233 @@
 from unittest import mock
 
 import pytest
+import yaml
 
+from cachi2.core.errors import PackageManagerError, PackageRejected
+from cachi2.core.package_managers.rpm import fetch_rpm_source
+from cachi2.core.package_managers.rpm.main import DEFAULT_LOCKFILE_NAME, _resolve_rpm_project
 from cachi2.core.package_managers.rpm.redhat import RedhatRpmsLock
+from cachi2.core.rooted_path import RootedPath
+
+
+@mock.patch("cachi2.core.package_managers.rpm.main.RequestOutput.from_obj_list")
+@mock.patch("cachi2.core.package_managers.rpm.main._resolve_rpm_project")
+def test_fetch_rpm_source(
+    mock_resolve_rpm_project: mock.Mock,
+    mock_from_obj_list: mock.Mock,
+) -> None:
+    mock_component = mock.Mock()
+    mock_resolve_rpm_project.return_value = [mock_component]
+    mock_request = mock.Mock()
+    mock_request.rpm_packages = [mock.Mock()]
+    fetch_rpm_source(mock_request)
+    mock_resolve_rpm_project.assert_called_once()
+    mock_from_obj_list.assert_called_once_with(
+        components=[mock_component], environment_variables=[], project_files=[]
+    )
+
+
+def test_resolve_rpm_project_no_lockfile(rooted_tmp_path: RootedPath) -> None:
+    with pytest.raises(PackageRejected) as exc_info:
+        mock_source_dir = mock.Mock()
+        mock_source_dir.join_within_root.return_value.path.exists.return_value = False
+        _resolve_rpm_project(mock_source_dir, mock.Mock())
+    assert f"RPM lockfile '{DEFAULT_LOCKFILE_NAME}' missing, refusing to continue" in str(
+        exc_info.value
+    )
+
+
+def test_resolve_rpm_project_invalid_yaml_format(rooted_tmp_path: RootedPath) -> None:
+    with open(rooted_tmp_path.join_within_root("rpms.lock.yaml"), "w") as f:
+        # colon is missing at the end
+        f.write("lockfileVendor: redhat\nlockfileVersion: 1\narches\n")
+    with pytest.raises(PackageRejected) as exc_info:
+        _resolve_rpm_project(rooted_tmp_path, rooted_tmp_path)
+
+    with open(rooted_tmp_path.join_within_root("rpms.lock.yaml"), "w") as f:
+        # end of line is missing between items
+        f.write("lockfileVendor: redhat lockfileVersion: 1\narches:\n")
+    with pytest.raises(PackageRejected) as exc_info:
+        _resolve_rpm_project(rooted_tmp_path, rooted_tmp_path)
+    assert f"RPM lockfile '{DEFAULT_LOCKFILE_NAME}' yaml format is not correct" in str(
+        exc_info.value
+    )
+
+
+def test_resolve_rpm_project_invalid_lockfile_format(rooted_tmp_path: RootedPath) -> None:
+    with open(rooted_tmp_path.join_within_root("rpms.lock.yaml"), "w") as f:
+        yaml.safe_dump(
+            {
+                "lockfileVendor": "unknown",
+                "lockfileVersion": 1,
+                "arches": [],
+            },
+            f,
+        )
+    with pytest.raises(PackageManagerError) as exc_info:
+        _resolve_rpm_project(rooted_tmp_path, rooted_tmp_path)
+
+    with open(rooted_tmp_path.join_within_root("rpms.lock.yaml"), "w") as f:
+        yaml.safe_dump(
+            {
+                "lockfileVendor": "redhat",
+                "lockfileVersion": 2,
+                "arches": [],
+            },
+            f,
+        )
+    with pytest.raises(PackageManagerError) as exc_info:
+        _resolve_rpm_project(rooted_tmp_path, rooted_tmp_path)
+
+    with open(rooted_tmp_path.join_within_root("rpms.lock.yaml"), "w") as f:
+        yaml.safe_dump(
+            {
+                "lockfileVendor": "redhat",
+                "lockfileVersion": "zz",
+                "arches": [],
+            },
+            f,
+        )
+    with pytest.raises(PackageManagerError) as exc_info:
+        _resolve_rpm_project(rooted_tmp_path, rooted_tmp_path)
+
+    with open(rooted_tmp_path.join_within_root("rpms.lock.yaml"), "w") as f:
+        yaml.safe_dump(
+            {
+                "vendor": "redhat",
+                "lockfileVersion": 1,
+                "arches": [],
+            },
+            f,
+        )
+    with pytest.raises(PackageManagerError) as exc_info:
+        _resolve_rpm_project(rooted_tmp_path, rooted_tmp_path)
+
+    with open(rooted_tmp_path.join_within_root("rpms.lock.yaml"), "w") as f:
+        yaml.safe_dump(
+            {
+                "lockfileVendor": "redhat",
+                "lockfileVersion": 1,
+                "arches": "everything",
+            },
+            f,
+        )
+    with pytest.raises(PackageManagerError) as exc_info:
+        _resolve_rpm_project(rooted_tmp_path, rooted_tmp_path)
+
+    with open(rooted_tmp_path.join_within_root("rpms.lock.yaml"), "w") as f:
+        yaml.safe_dump(
+            {
+                "lockfileVendor": "redhat",
+                "lockfileVersion": "zz",
+                "arches": [
+                    {
+                        "arch": "x86_64",
+                        "packages": [
+                            {
+                                "address": "SOME_ADDRESS",
+                                "size": 1111,
+                            },
+                        ],
+                    },
+                ],
+            },
+            f,
+        )
+    with pytest.raises(PackageManagerError) as exc_info:
+        _resolve_rpm_project(rooted_tmp_path, rooted_tmp_path)
+    assert f"RPM lockfile '{DEFAULT_LOCKFILE_NAME}' format is not valid" in str(exc_info.value)
+
+
+def test_resolve_rpm_project_arch_empty(rooted_tmp_path: RootedPath) -> None:
+    with open(rooted_tmp_path.join_within_root("rpms.lock.yaml"), "w") as f:
+        yaml.safe_dump(
+            {
+                "lockfileVendor": "redhat",
+                "lockfileVersion": 1,
+                "arches": [
+                    {
+                        "arch": "x86_64",
+                    },
+                ],
+            },
+            f,
+        )
+    with pytest.raises(PackageManagerError) as exc_info:
+        _resolve_rpm_project(rooted_tmp_path, rooted_tmp_path)
+
+    with open(rooted_tmp_path.join_within_root("rpms.lock.yaml"), "w") as f:
+        yaml.safe_dump(
+            {
+                "lockfileVendor": "redhat",
+                "lockfileVersion": 1,
+                "arches": [
+                    {
+                        "arch": "aarch64",
+                        "packages": [],
+                    },
+                ],
+            },
+            f,
+        )
+    with pytest.raises(PackageManagerError) as exc_info:
+        _resolve_rpm_project(rooted_tmp_path, rooted_tmp_path)
+
+    with open(rooted_tmp_path.join_within_root("rpms.lock.yaml"), "w") as f:
+        yaml.safe_dump(
+            {
+                "lockfileVendor": "redhat",
+                "lockfileVersion": 1,
+                "arches": [
+                    {
+                        "arch": "i686",
+                        "packages": [],
+                        "source": [],
+                    },
+                    {
+                        "arch": "x86_64",
+                        "packages": [
+                            {
+                                "url": "SOME_URL",
+                            },
+                        ],
+                    },
+                ],
+            },
+            f,
+        )
+    with pytest.raises(PackageManagerError) as exc_info:
+        _resolve_rpm_project(rooted_tmp_path, rooted_tmp_path)
+    assert "At least one field ('packages', 'source') must be set in every arch." in str(
+        exc_info.value
+    )
+
+
+def test_resolve_rpm_project_correct_format(rooted_tmp_path: RootedPath) -> None:
+    with open(rooted_tmp_path.join_within_root("rpms.lock.yaml"), "w") as f:
+        yaml.safe_dump(
+            {
+                "lockfileVendor": "redhat",
+                "lockfileVersion": 1,
+                "arches": [
+                    {
+                        "arch": "x86_64",
+                        "packages": [
+                            {
+                                "url": "SOME_URL",
+                            },
+                        ],
+                        "source": [
+                            {
+                                "url": "SOME_URL",
+                            },
+                        ],
+                    },
+                ],
+            },
+            f,
+        )
+    _resolve_rpm_project(rooted_tmp_path, rooted_tmp_path)
 
 
 class TestRedhatRpmsLock:
