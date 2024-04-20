7dd99a894df674dc34cff3a5d2b8e07bb9a2d2dc
Bruno Pimentel <bpimente@redhat.com>
2024-04-20 15:49:42 -0300

    rpm tests: Add missing checksums integration test scenario
    Signed-off-by: Bruno Pimentel <bpimente@redhat.com>

--- tests/integration/test_rpm.py
+++ tests/integration/test_rpm.py
@@ -7,6 +7,48 @@ from . import utils
 
 
 @pytest.mark.parametrize(
+    "test_params",
+    [
+        pytest.param(
+            utils.TestParameters(
+                repo="https://github.com/cachito-testing/cachi2-rpm",
+                ref="3956e4d095920b3f06b861dbc778a520fdb89fd2",
+                packages=({"path": ".", "type": "rpm"},),
+                flags=["--dev-package-managers"],
+                check_output=True,
+                check_deps_checksums=False,
+                check_vendor_checksums=False,
+                expected_exit_code=0,
+            ),
+            id="rpm_missing_checksums",
+        ),
+    ],
+)
+def test_rpm_packages(
+    test_params: utils.TestParameters,
+    cachi2_image: utils.ContainerImage,
+    tmp_path: Path,
+    test_data_dir: Path,
+    request: pytest.FixtureRequest,
+) -> None:
+    """
+    Test fetched dependencies for RPMs.
+
+    :param test_params: Test case arguments
+    :param tmp_path: Temp directory for pytest
+    """
+    test_case = request.node.callspec.id
+
+    source_folder = utils.clone_repository(
+        test_params.repo, test_params.ref, f"{test_case}-source", tmp_path
+    )
+
+    utils.fetch_deps_and_check_output(
+        tmp_path, test_case, test_params, source_folder, test_data_dir, cachi2_image
+    )
+
+
+@pytest.mark.parametrize(
     "test_params, check_cmd, expected_cmd_output",
     [
         # Test case that checks fetching RPM files, generating repos and repofiles, building an
