containerbuildsystem/cachi2

commit 9967a2e8b04daa4134580f2e792c26ffe1c9a61e

Containerfile: Pre install Go 1.21.0 to the cachi2 image
Currently, we only pre install Go 1.20 as a fallback for projects
building with older version of Go due to some specific behavioral
semantic changes to 1.21 which our container image (Fedora 39) is based
on.
Starting Go 1.21, the go toolchain can fetch and use newer toolchains
than the bundled one which is used to process a project from the
command line. However, if the project's Go 1.21.X toolchain requirement
is older than the bundled toolchain doing the processing, the older
toolchain is never downloaded in such case. This design decision makes
things for hermetic builds harder because not having any control over
the container build environment it could potentially fail due a missing
Go toolchain version if we had not prefetched it due to the Go's
version handling.

Signed-off-by: Erik Skultety <eskultet@redhat.com>

Containerfile
@@ -34,10 +34,12 @@ RUN npm install && \
 #   - move the SDK to a host local install system-wide location
 #   - remove the shim as it forces and expects the SDK to be used from $HOME
 #   - clean any build artifacts Go creates as part of the process.
-RUN go install 'golang.org/dl/go1.20@latest' && \
-    "$HOME/go/bin/go1.20" download && \
-    mkdir -p /usr/local/go && \
-    mv "$HOME/sdk/go1.20" /usr/local/go && \
-    rm -rf "$HOME/go" "$HOME/.cache/go-build/"
+RUN for go_ver in "go1.20" "go1.21.0"; do \
+        go install "golang.org/dl/${go_ver}@latest" && \
+        "$HOME/go/bin/$go_ver" download && \
+        mkdir -p /usr/local/go && \
+        mv "$HOME/sdk/$go_ver" /usr/local/go && \
+        rm -rf "$HOME/go" "$HOME/.cache/go-build/"; \
+    done
 
 ENTRYPOINT ["cachi2"]

commit c6a4ac391c88d3ef8a3911095e10edf82a36e2f0

Containerfile: Update commentary on the Go SDK installs
Commit 9967a2e8 forgot to update the commentary when adding another
fixed version of Go SDK to download and install during the image build.

Fixes: 9967a2e8b04daa4134580f2e792c26ffe1c9a61e

Signed-off-by: Erik Skultety <eskultet@redhat.com>

Containerfile
@@ -28,7 +28,7 @@ RUN npm install && \
     corepack enable yarn && \
     microdnf -y remove nodejs-npm
 
-# Install an older version of Go fixed at 1.20 (along with the base >=1.21):
+# Manual install of specific fixed Go SDK versions (1.20 & 1.21.0):
 #   - install Go's official shim
 #   - let the shim download the actual Go SDK (the download forces the output parent dir to $HOME)
 #   - move the SDK to a host local install system-wide location