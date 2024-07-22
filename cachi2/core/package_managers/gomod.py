containerbuildsystem/cachi2

commit 2c83a586eab87487b1f2303f0c037748b2603059

package_managers: gomod: Parse the toolchain keyword from go.mod
Unfortunately, with Go 1.21 it's not enough to rely solely on what the
mod file declares with the 'go' line, but also what it declares with
the 'toolchain' line. While 'toolchain' is only suggestive, i.e. not
usually enforced, the situation changes when combined with
GOTOOLCHAIN=auto. Start parsing that keyword for future use.

Signed-off-by: Erik Skultety <eskultet@redhat.com>

cachi2/core/package_managers/gomod.py
@@ -653,7 +653,7 @@ def _get_repository_name(source_dir: RootedPath) -> str:
     return f"{url.hostname}{url.path.rstrip('/').removesuffix('.git')}"
 
 
-def _get_gomod_version(go_mod_file: RootedPath) -> Optional[str]:
+def _get_gomod_version(go_mod_file: RootedPath) -> Tuple[Optional[str], Optional[str]]:
     """Return the required/recommended version of Go from go.mod.
 
     We need to extract the desired version of Go ourselves as older versions of Go might fail
@@ -663,12 +663,26 @@ def _get_gomod_version(go_mod_file: RootedPath) -> Optional[str]:
     If we cannot extract a version from the 'go' line, we return None, leaving it up to the caller
     to decide what to do next.
     """
+    go_version = None
+    toolchain_version = None
+
+    go_version_regex = r"^\s*go\s+(?P<ver>\d\.\d+(:?\.\d+)?)\s*$"
+    toolchain_version_regex = r"^\s*toolchain\s+go(?P<ver>\d\.\d+(:?\.\d+)?)\s*$"
+
+    go_pattern = re.compile(go_version_regex)
+    toolchain_pattern = re.compile(toolchain_version_regex)
+
     with open(go_mod_file) as f:
-        reg = re.compile(r"^\s*go\s+(?P<ver>\d\.\d+(:?.\d+)?)\s*$")
         for line in f:
-            if match := re.match(reg, line):
-                return match.group("ver")
-    return None
+            if not go_version and (match := re.match(go_pattern, line)):
+                go_version = match.group("ver")
+                continue
+
+            if not toolchain_version and (match := re.match(toolchain_pattern, line)):
+                toolchain_version = match.group("ver")
+                continue
+
+    return (go_version, toolchain_version)
 
 
 def _protect_against_symlinks(app_dir: RootedPath) -> None:
@@ -722,26 +736,41 @@ def _find_missing_gomod_files(source_path: RootedPath, subpaths: list[str]) -> l
 
 def _setup_go_toolchain(go_mod_file: RootedPath) -> Go:
     go = Go()
+    target_version = None
     go1_21 = version.Version("1.21")
     go_base_version = go.version
-    go_mod_version_msg = "go.mod recommends/requires Go version: {}"
+    go_mod_version_msg = "go.mod reported versions: '{}'[go], '{}'[toolchain]"
 
-    if (_ := _get_gomod_version(go_mod_file)) is None:
+    go_version_str, toolchain_version_str = _get_gomod_version(go_mod_file)
+    if not go_version_str:
         # Go added the 'go' directive to go.mod in 1.12 [1]. If missing, 1.16 is assumed [2].
         # For our version comparison purposes we set the version explicitly to 1.20 if missing.
         # [1] https://go.dev/doc/go1.12#modules
         # [2] https://go.dev/ref/mod#go-mod-file-go
-        _ = "1.20"
-        go_mod_version_msg += " " + "(cachi2 enforced)"
+        go_version_str = "1.20"
+        go_mod_version_msg += " (cachi2 enforced)"
 
-    go_mod_version = version.Version(_)
+    log.info(
+        go_mod_version_msg.format(
+            go_version_str, toolchain_version_str if toolchain_version_str else "-"
+        )
+    )
 
-    log.info(go_mod_version_msg.format(go_mod_version))
+    if not toolchain_version_str:
+        toolchain_version_str = go_version_str
 
-    if go_mod_version >= go1_21 and go_base_version < go1_21:
+    go_mod_version = version.Version(go_version_str)
+    go_mod_toolchain_version = version.Version(toolchain_version_str)
+
+    if go_mod_version >= go_mod_toolchain_version:
+        target_version = go_mod_version
+    else:
+        target_version = go_mod_toolchain_version
+
+    if target_version >= go1_21 and go_base_version < go1_21:
         # our base Go installation is too old and we need a newer one to support new keywords
         go = Go(release="go1.21.0")
-    elif go_mod_version < go1_21 and go_base_version >= go1_21:
+    elif target_version < go1_21 and go_base_version >= go1_21:
         # Starting with Go 1.21, Go doesn't try to be semantically backwards compatible in that the
         # 'go X.Y' line now denotes the minimum required version of Go, no a "suggested" version.
         # What it means in practice is that a Go toolchain >= 1.21 enforces the biggest

tests/unit/package_managers/test_gomod.py
@@ -182,7 +182,7 @@ def test_resolve_gomod(
 
     mock_version_resolver.get_golang_version.return_value = "v0.1.0"
     mock_go_release.return_value = "go0.1.0"
-    mock_get_gomod_version.return_value = "0.1.1"
+    mock_get_gomod_version.return_value = ("0.1.1", "0.1.2")
 
     flags: list[Flag] = []
     if cgo_disable:
@@ -282,7 +282,7 @@ def test_resolve_gomod_vendor_dependencies(
 
     mock_version_resolver.get_golang_version.return_value = "v0.1.0"
     mock_go_release.return_value = "go0.1.0"
-    mock_get_gomod_version.return_value = "0.1.1"
+    mock_get_gomod_version.return_value = ("0.1.1", "0.1.2")
 
     flags: list[Flag] = ["gomod-vendor"]
     if force_gomod_tidy:
@@ -332,7 +332,7 @@ def test_resolve_gomod_vendor_without_flag(
     module_dir.path.joinpath("vendor").mkdir(parents=True)
     version_resolver = mock.Mock()
     mock_go_release.return_value = "go0.1.0"
-    mock_get_gomod_version.return_value = "0.1.1"
+    mock_get_gomod_version.return_value = ("0.1.1", "0.1.2")
 
     expected_error = (
         'The "gomod-vendor" or "gomod-vendor-check" flag must be set when your repository has '
@@ -394,7 +394,7 @@ def test_resolve_gomod_no_deps(
 
     mock_version_resolver.get_golang_version.return_value = "v2.1.1"
     mock_go_release.return_value = "go2.1.0"
-    mock_get_gomod_version.return_value = "2.1.1"
+    mock_get_gomod_version.return_value = ("2.1.1", "2.1.2")
 
     if force_gomod_tidy:
         gomod_request.flags = frozenset({"force-gomod-tidy"})
@@ -801,7 +801,7 @@ def test_go_list_cmd_failure(
 
     mock_config.return_value.gomod_download_max_tries = 1
     mock_go_release.return_value = "go0.1.0"
-    mock_get_gomod_version.return_value = "0.1.1"
+    mock_get_gomod_version.return_value = ("0.1.1", "0.1.2")
 
     # Mock the "subprocess.run" calls
     mock_run.side_effect = [
@@ -1581,23 +1581,41 @@ def test_fetch_tags_fail(repo_remote_with_tag: tuple[RootedPath, RootedPath]) ->
 
 
 @pytest.mark.parametrize(
-    "go_mod_file, go_mod_version",
-    [("go 1.21", "1.21"), ("    go    1.21.4    ", "1.21.4")],
+    "go_mod_file, go_mod_version, go_toolchain_version",
+    [
+        pytest.param("go 1.21", "1.21", None, id="no_toolchain"),
+        pytest.param("    go    1.21.4    ", "1.21.4", None, id="whitechars_no_toolchain"),
+        pytest.param("   toolchain   go1.21.4  ", None, "1.21.4", id="whitechars_toolchain"),
+        pytest.param("toolchain go1.21", None, "1.21", id="toolchain_missing_micro_version"),
+        pytest.param("go 1.21\ntoolchain go1.21.6", "1.21", "1.21.6", id="go_and_toolchain"),
+    ],
     indirect=["go_mod_file"],
 )
 def test_get_gomod_version(
-    rooted_tmp_path: RootedPath, go_mod_file: Path, go_mod_version: str
+    rooted_tmp_path: RootedPath, go_mod_file: Path, go_mod_version: str, go_toolchain_version: str
 ) -> None:
-    assert _get_gomod_version(rooted_tmp_path.join_within_root("go.mod")) == go_mod_version
+    assert _get_gomod_version(rooted_tmp_path.join_within_root("go.mod")) == (
+        go_mod_version,
+        go_toolchain_version,
+    )
+
+
+INVALID_VERSION_STRINGS = [
+    "go1.21",  # missing space between go and version number
+    "go 1.21.0.100",  # non-conforming to the X.Y(.Z)? versioning template
+    "1.21",  # missing 'go' at the beginning
+    "go 1.21 foo",  # extra characters after version string
+    "toolchain 1.21",  # missing 'go' prefix for the toolchain spec
+]
 
 
 @pytest.mark.parametrize(
     "go_mod_file",
-    [pytest.param(_, id=_) for _ in ["go1.21", "go 1.21.0.100", "1.21", "go 1.21 foo"]],
+    [pytest.param(_, id=_) for _ in INVALID_VERSION_STRINGS],
     indirect=True,
 )
 def test_get_gomod_version_fail(rooted_tmp_path: RootedPath, go_mod_file: Path) -> None:
-    assert _get_gomod_version(rooted_tmp_path.join_within_root("go.mod")) is None
+    assert _get_gomod_version(rooted_tmp_path.join_within_root("go.mod")) == (None, None)
 
 
 @pytest.mark.parametrize(

commit 91539fcb08e30b9013c1f566b3d508b43bccfe69

package_managers: gomod: Fix reporting of the parsed version from go.mod
We currently first set the fallback version string to 1.20 if for any
reason we can't parse the version string out of go.mod (e.g. the 'go'
line is missing with ancient Go releases or our regex is incomplete).
That's wrong and it can lead to nasty bugs where (without any further
debug logs) it's quite difficult to hunt down the exact issue why we
may be not be able to process a Go project. Fix this by reporting the
real version values parsed from the go.mod file and only then report a
potential fallback to using a different Go version.

Fixes: 2c83a586eab87487b1f2303f0c037748b2603059

Signed-off-by: Erik Skultety <eskultet@redhat.com>

cachi2/core/package_managers/gomod.py
@@ -750,22 +750,22 @@ def _setup_go_toolchain(go_mod_file: RootedPath) -> Go:
     target_version = None
     go_max_version = version.Version("1.21")
     go_base_version = go.version
-    go_mod_version_msg = "go.mod reported versions: '{}'[go], '{}'[toolchain]"
+    go_mod_version_msg = "go.mod reported versions: '%s'[go], '%s'[toolchain]"
 
     go_version_str, toolchain_version_str = _get_gomod_version(go_mod_file)
+    log.info(
+        go_mod_version_msg,
+        go_version_str if go_version_str else "-",
+        toolchain_version_str if toolchain_version_str else "-",
+    )
+
     if not go_version_str:
         # Go added the 'go' directive to go.mod in 1.12 [1]. If missing, 1.16 is assumed [2].
         # For our version comparison purposes we set the version explicitly to 1.20 if missing.
         # [1] https://go.dev/doc/go1.12#modules
         # [2] https://go.dev/ref/mod#go-mod-file-go
         go_version_str = "1.20"
-        go_mod_version_msg += " (cachi2 enforced)"
-
-    log.info(
-        go_mod_version_msg.format(
-            go_version_str, toolchain_version_str if toolchain_version_str else "-"
-        )
-    )
+        log.debug("Could not parse Go version from go.mod, using %s as fallback", go_version_str)
 
     if not toolchain_version_str:
         toolchain_version_str = go_version_str