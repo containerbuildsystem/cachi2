24103c39c2c8001f80d3e4904a695271f94a304d
Taylor Madore <tmadore@redhat.com>
2024-07-26 13:24:53 -0400

    prefetch yarn-classic using offline mirror
    Resolves #626
    
    Prefetching dependencies for Yarn 1.x will be done using Yarn's
    offline mirror feature. When a project is configured to use the offline
    mirror, Yarn will store compressed archives in the mirror directory
    and can install cached project dependencies from there later without
    network access.
    
    See https://classic.yarnpkg.com/blog/2016/11/24/offline-mirror/
    
    Command: `yarn install`
    
    Options:
    - non-interactive: Disable interactive prompts
    - frozen-lockfile: Fail if yarn.lock updates are needed
    - disable-pnp: Disable Plug'n'Play installation
    - no-default-rc: Prevent Yarn from automatically detecting .yarnrc and .npmrc files
    - ignore-engines: Ignore the engine node version during the prefetch
    
    The behavior of Yarn can also be configured using select environment variables.
    The following ones should be set for the prefetch:
    - COREPACK_ENABLE_DOWNLOAD_PROMPT=0 (Prevent corepack from prompting for
      interactive input)
    - COREPACK_ENABLE_PROJECT_SPEC=0 (Ignore packageManager field in
      package.json and use the default Yarn 1.x version)
    - YARN_YARN_OFFLINE_MIRROR=<absolute path to the request output directory>
    - YARN_YARN_OFFLINE_MIRROR_PRUNING=false
    - YARN_IGNORE_PATH=true (Use the corepack yarn version, not any Yarn version specified by the user)
    - YARN_IGNORE_SCRIPTS=true (Do not execute lifecycle scripts, which would
      be arbitrary code execution)
    
    Signed-off-by: Taylor Madore <tmadore@redhat.com>

--- cachi2/core/package_managers/yarn_classic/main.py
+++ cachi2/core/package_managers/yarn_classic/main.py
@@ -1,5 +1,9 @@
 from cachi2.core.models.input import Request
 from cachi2.core.models.output import Component, EnvironmentVariable, RequestOutput
+from cachi2.core.package_managers.yarn.utils import run_yarn_cmd
+from cachi2.core.rooted_path import RootedPath
+
+MIRROR_DIR = "deps/yarn-classic"
 
 
 def fetch_yarn_source(request: Request) -> RequestOutput:
@@ -7,7 +11,48 @@ def fetch_yarn_source(request: Request) -> RequestOutput:
     components: list[Component] = []
     env_vars: list[EnvironmentVariable] = []
 
+    def _ensure_mirror_dir_exists(output_dir: RootedPath) -> None:
+        output_dir.join_within_root(MIRROR_DIR).path.mkdir(parents=True, exist_ok=True)
+
+    for package in request.yarn_classic_packages:
+        path = request.source_dir.join_within_root(package.path)
+        _ensure_mirror_dir_exists(request.output_dir)
+        _fetch_dependencies(path, _get_prefetch_environment_variables(request.output_dir))
+
     return RequestOutput.from_obj_list(components, env_vars, project_files=[])
+
+
+def _fetch_dependencies(source_dir: RootedPath, env: dict[str, str]) -> None:
+    """Fetch dependencies using 'yarn install'.
+
+    :param source_dir: the directory in which the yarn command will be called.
+    :param env: environment variable mapping used for the prefetch.
+    :raises PackageManagerError: if the 'yarn install' command fails.
+    """
+    run_yarn_cmd(
+        [
+            "install",
+            "--disable-pnp",
+            "--frozen-lockfile",
+            "--ignore-engines",
+            "--no-default-rc",
+            "--non-interactive",
+        ],
+        source_dir,
+        env,
+    )
+
+
+def _get_prefetch_environment_variables(output_dir: RootedPath) -> dict[str, str]:
+    """Get environment variables that will be used for the prefetch."""
+    return {
+        "COREPACK_ENABLE_DOWNLOAD_PROMPT": "0",
+        "COREPACK_ENABLE_PROJECT_SPEC": "0",
+        "YARN_IGNORE_PATH": "true",
+        "YARN_IGNORE_SCRIPTS": "true",
+        "YARN_YARN_OFFLINE_MIRROR": str(output_dir.join_within_root(MIRROR_DIR)),
+        "YARN_YARN_OFFLINE_MIRROR_PRUNING": "false",
+    }
 
 
 def _generate_build_environment_variables() -> list[EnvironmentVariable]:
--- tests/unit/package_managers/yarn_classic/test_main.py
+++ tests/unit/package_managers/yarn_classic/test_main.py
@@ -1,12 +1,18 @@
+from pathlib import Path
+from unittest import mock
+
 import pytest
 
 from cachi2.core.models.input import Request
 from cachi2.core.models.output import BuildConfig, EnvironmentVariable, RequestOutput
 from cachi2.core.models.sbom import Component
 from cachi2.core.package_managers.yarn_classic.main import (
+    _fetch_dependencies,
     _generate_build_environment_variables,
+    _get_prefetch_environment_variables,
     fetch_yarn_source,
 )
+from cachi2.core.rooted_path import RootedPath
 
 
 @pytest.fixture(scope="module")
@@ -34,13 +40,76 @@ def test_generate_build_environment_variables(
             [],
             id="single_input_package",
         ),
+        pytest.param(
+            [{"type": "yarn-classic", "path": "."}, {"type": "yarn-classic", "path": "./path"}],
+            [],
+            id="multiple_input_packages",
+        ),
     ],
     indirect=["input_request"],
 )
-def test_fetch_yarn_source(input_request: Request, components: list[Component]) -> None:
-    output = fetch_yarn_source(input_request)
+@mock.patch("cachi2.core.package_managers.yarn_classic.main._get_prefetch_environment_variables")
+@mock.patch("cachi2.core.package_managers.yarn_classic.main._fetch_dependencies")
+def test_fetch_yarn_source(
+    mock_fetch_dependencies: mock.Mock,
+    mock_prefetch_env_vars: mock.Mock,
+    input_request: Request,
+    components: list[Component],
+) -> None:
     expected_output = RequestOutput(
         components=components,
         build_config=BuildConfig(environment_variables=[]),
     )
+
+    output = fetch_yarn_source(input_request)
+
+    mock_prefetch_env_vars.assert_has_calls(
+        [mock.call(input_request.output_dir) for _ in input_request.packages]
+    )
+
+    calls = []
+    for package in input_request.packages:
+        package_path = input_request.source_dir.join_within_root(package.path)
+        calls.append(mock.call(package_path, mock_prefetch_env_vars(input_request.output_dir)))
+    mock_fetch_dependencies.assert_has_calls(calls)
+
+    assert input_request.output_dir.join_within_root("deps/yarn-classic").path.exists()
+    assert output == expected_output
+
+
+@mock.patch("cachi2.core.package_managers.yarn_classic.main.run_yarn_cmd")
+def test_fetch_dependencies(mock_run_yarn_cmd: mock.Mock, tmp_path: Path) -> None:
+    env = {"foo": "bar"}
+    rooted_tmp_path = RootedPath(tmp_path)
+
+    _fetch_dependencies(rooted_tmp_path, env)
+
+    mock_run_yarn_cmd.assert_called_with(
+        [
+            "install",
+            "--disable-pnp",
+            "--frozen-lockfile",
+            "--ignore-engines",
+            "--no-default-rc",
+            "--non-interactive",
+        ],
+        rooted_tmp_path,
+        env,
+    )
+
+
+def test_get_prefetch_environment_variables(tmp_path: Path) -> None:
+    request_output_dir = RootedPath(tmp_path).join_within_root("output")
+    yarn_deps_dir = request_output_dir.join_within_root("deps/yarn-classic")
+    expected_output = {
+        "COREPACK_ENABLE_DOWNLOAD_PROMPT": "0",
+        "COREPACK_ENABLE_PROJECT_SPEC": "0",
+        "YARN_IGNORE_PATH": "true",
+        "YARN_IGNORE_SCRIPTS": "true",
+        "YARN_YARN_OFFLINE_MIRROR": str(yarn_deps_dir),
+        "YARN_YARN_OFFLINE_MIRROR_PRUNING": "false",
+    }
+
+    output = _get_prefetch_environment_variables(request_output_dir)
+
     assert output == expected_output
