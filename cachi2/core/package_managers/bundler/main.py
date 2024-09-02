0e65b37815f0d342830ecbe52a78b07fb6ff1b7e
Michal Šoltis <msoltis@redhat.com>
2024-09-02 16:43:41 +0200

    bundler: Implement parsing Gemfile.lock
    Parsing logic is done natively using ruby standard library.
    
    This commit runs the ruby script as a subprocess
    and consumes generated JSON into our custom pydantic
    classes and validates potential errors.
    
    Unit tests cover scenarios:
    - test if Gemfile or Gemfile.lock are missing
    - test invalid formats of Gemfile.lock
    - test valid format of Gemfile.lock
    
    Tests that depend on ruby runtime use
    mocked JSON file (that would be returned
    by ruby script) to avoid failures when
    running them locally.
    
    Signed-off-by: Michal Šoltis <msoltis@redhat.com>

--- cachi2/core/package_managers/bundler/main.py
+++ cachi2/core/package_managers/bundler/main.py
@@ -1,6 +1,8 @@
 from cachi2.core.models.input import Request
 from cachi2.core.models.output import EnvironmentVariable, ProjectFile, RequestOutput
 from cachi2.core.models.sbom import Component
+from cachi2.core.package_managers.bundler.parser import parse_lockfile
+from cachi2.core.rooted_path import RootedPath
 
 
 def fetch_bundler_source(request: Request) -> RequestOutput:
@@ -9,8 +11,18 @@ def fetch_bundler_source(request: Request) -> RequestOutput:
     environment_variables: list[EnvironmentVariable] = []
     project_files: list[ProjectFile] = []
 
+    for package in request.packages:
+        path_within_root = request.source_dir.join_within_root(package.path)
+        _resolve_bundler_package(package_dir=path_within_root, output_dir=request.output_dir)
+
     return RequestOutput.from_obj_list(
         components=components,
         environment_variables=environment_variables,
         project_files=project_files,
     )
+
+
+def _resolve_bundler_package(package_dir: RootedPath, output_dir: RootedPath) -> list[Component]:
+    """Process a request for a single bundler package."""
+    parse_lockfile(package_dir)
+    return []
---
+++ tests/unit/package_managers/bundler/test_parser.py
@@ -0,0 +1,182 @@
+import json
+import subprocess
+from copy import deepcopy
+from typing import Any
+from unittest import mock
+
+import pydantic
+import pytest
+
+from cachi2.core.errors import PackageManagerError, PackageRejected, UnexpectedFormat
+from cachi2.core.package_managers.bundler.parser import (
+    GEMFILE,
+    GEMFILE_LOCK,
+    GemDependency,
+    GitDependency,
+    PathDependency,
+    parse_lockfile,
+)
+from cachi2.core.rooted_path import RootedPath
+
+
+@pytest.fixture
+def empty_bundler_files(rooted_tmp_path: RootedPath) -> tuple[RootedPath, RootedPath]:
+    gemfile_path = rooted_tmp_path.join_within_root(GEMFILE)
+    gemfile_path.path.touch()
+
+    lockfile_path = rooted_tmp_path.join_within_root(GEMFILE_LOCK)
+    lockfile_path.path.touch()
+
+    return gemfile_path, lockfile_path
+
+
+SAMPLE_PARSER_OUTPUT = {
+    "bundler_version": "2.5.10",
+    "dependencies": [{"name": "example", "version": "0.1.0"}],
+}
+
+
+@pytest.fixture
+def sample_parser_output() -> dict[str, Any]:
+    return deepcopy(SAMPLE_PARSER_OUTPUT)
+
+
+def test_parse_lockfile_without_bundler_files(rooted_tmp_path: RootedPath) -> None:
+    with pytest.raises(PackageRejected) as exc_info:
+        parse_lockfile(rooted_tmp_path)
+
+    assert (
+        "Gemfile and Gemfile.lock must be present in the package directory"
+        in exc_info.value.friendly_msg()
+    )
+
+
+@mock.patch("cachi2.core.package_managers.bundler.parser.run_cmd")
+def test_parse_lockfile_os_error(
+    mock_run_cmd: mock.MagicMock,
+    empty_bundler_files: tuple[RootedPath, RootedPath],
+    rooted_tmp_path: RootedPath,
+) -> None:
+    mock_run_cmd.side_effect = subprocess.CalledProcessError(returncode=1, cmd="cmd")
+
+    with pytest.raises(PackageManagerError) as exc_info:
+        parse_lockfile(rooted_tmp_path)
+
+    assert f"Failed to parse {empty_bundler_files[1]}" in exc_info.value.friendly_msg()
+
+
+@mock.patch("cachi2.core.package_managers.bundler.parser.run_cmd")
+@pytest.mark.parametrize(
+    "error, expected_error_msg",
+    [
+        ("LOCKFILE_INVALID_URL", "Input should be a valid URL"),
+        ("LOCKFILE_INVALID_URL_SCHEME", "URL scheme should be 'https'"),
+        ("LOCKFILE_INVALID_REVISION", "String should match pattern '^[a-fA-F0-9]{40}$'"),
+        ("LOCKFILE_INVALID_PATH", "PATH dependencies should be within the package root"),
+    ],
+)
+def test_parse_lockfile_invalid_format(
+    mock_run_cmd: mock.MagicMock,
+    error: str,
+    expected_error_msg: str,
+    empty_bundler_files: tuple[RootedPath, RootedPath],
+    sample_parser_output: dict[str, Any],
+    rooted_tmp_path: RootedPath,
+) -> None:
+    if error == "LOCKFILE_INVALID_URL":
+        sample_parser_output["dependencies"][0].update(
+            {
+                "type": "git",
+                "url": "github",
+                "ref": "f" * 40,
+            }
+        )
+    elif error == "LOCKFILE_INVALID_URL_SCHEME":
+        sample_parser_output["dependencies"][0].update(
+            {
+                "type": "git",
+                "url": "http://github.com/3scale/json-schema.git",
+                "ref": "f" * 40,
+            }
+        )
+    elif error == "LOCKFILE_INVALID_REVISION":
+        sample_parser_output["dependencies"][0].update(
+            {
+                "type": "git",
+                "url": "https://github.com/3scale/json-schema.git",
+                "ref": "abcd",
+            }
+        )
+    elif error == "LOCKFILE_INVALID_PATH":
+        sample_parser_output["dependencies"][0].update(
+            {
+                "type": "path",
+                "path": "/root/pathgem",
+            }
+        )
+
+    mock_run_cmd.return_value = json.dumps(sample_parser_output)
+    with pytest.raises((pydantic.ValidationError, UnexpectedFormat)) as exc_info:
+        parse_lockfile(rooted_tmp_path)
+
+    assert expected_error_msg in str(exc_info.value)
+
+
+@mock.patch("cachi2.core.package_managers.bundler.parser.run_cmd")
+def test_parse_gemlock(
+    mock_run_cmd: mock.MagicMock,
+    empty_bundler_files: tuple[RootedPath, RootedPath],
+    sample_parser_output: dict[str, Any],
+    rooted_tmp_path: RootedPath,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    base_dep: dict[str, str] = sample_parser_output["dependencies"][0]
+    sample_parser_output["dependencies"] = [
+        {
+            "type": "git",
+            "url": "https://github.com/3scale/json-schema.git",
+            "ref": "f" * 40,
+            **base_dep,
+        },
+        {
+            "type": "path",
+            "path": "subpath/pathgem",
+            **base_dep,
+        },
+        {
+            "type": "rubygems",
+            "source": "https://rubygems.org/",
+            **base_dep,
+        },
+    ]
+
+    mock_run_cmd.return_value = json.dumps(sample_parser_output)
+    result = parse_lockfile(rooted_tmp_path)
+
+    expected_deps = [
+        GitDependency(
+            name="example",
+            version="0.1.0",
+            url="https://github.com/3scale/json-schema.git",
+            ref="f" * 40,
+        ),
+        PathDependency(name="example", version="0.1.0", path="subpath/pathgem"),
+        GemDependency(name="example", version="0.1.0", source="https://rubygems.org/"),
+    ]
+
+    assert f"Package {rooted_tmp_path.path.name} is bundled with version 2.5.10" in caplog.messages
+    assert result == expected_deps
+
+
+@mock.patch("cachi2.core.package_managers.bundler.parser.run_cmd")
+def test_parse_gemlock_empty(
+    mock_run_cmd: mock.MagicMock,
+    empty_bundler_files: tuple[RootedPath, RootedPath],
+    rooted_tmp_path: RootedPath,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    mock_run_cmd.return_value = '{"bundler_version": "2.5.10", "dependencies": []}'
+    result = parse_lockfile(rooted_tmp_path)
+
+    assert f"Package {rooted_tmp_path.path.name} is bundled with version 2.5.10" in caplog.messages
+    assert result == []
