7e1bd60d5bf21087b067b676e50598da3dc5d5ef
Michal Šoltis <msoltis@redhat.com>
2024-09-02 16:43:41 +0200

    bundler: Implement parsing Gemfile.lock
    Parsing logic is done natively using ruby standard library.
    
    This commit runs the ruby script as a subprocess
    and consumes generated JSON into our custom pydantic
    classes and validates potential errors.
    
    Unit tests cover scenarios:
    - test if Gemfile or Gemfile.lock are missing
    - test invalid formats of Gemfile.lock
    - test valid format of Gemfile.lock
    
    Tests that depend on ruby runtime use
    mocked JSON file (that would be returned
    by ruby script) to avoid failures when
    running them locally.
    
    Signed-off-by: Michal Šoltis <msoltis@redhat.com>

---
+++ cachi2/core/package_managers/bundler/parser.py
@@ -0,0 +1,127 @@
+import json
+import logging
+import subprocess
+from pathlib import Path
+from typing import Annotated, Optional, Union
+
+import pydantic
+
+from cachi2.core.errors import PackageManagerError, PackageRejected, UnexpectedFormat
+from cachi2.core.rooted_path import PathOutsideRoot, RootedPath
+from cachi2.core.utils import run_cmd
+
+log = logging.getLogger(__name__)
+
+GEMFILE = "Gemfile"
+GEMFILE_LOCK = "Gemfile.lock"
+
+AcceptedUrl = Annotated[
+    pydantic.HttpUrl,
+    pydantic.UrlConstraints(allowed_schemes=["https"]),
+]
+
+AcceptedGitRef = Annotated[
+    pydantic.StrictStr,
+    pydantic.StringConstraints(pattern=r"^[a-fA-F0-9]{40}$"),
+]
+
+
+class _GemMetadata(pydantic.BaseModel):
+    """
+    Base class for gem metadata.
+
+    Attributes:
+        name:       The name of the gem.
+        version:    The version of the gem.
+    """
+
+    name: str
+    version: str
+
+
+class GemDependency(_GemMetadata):
+    """
+    Represents a gem dependency.
+
+    Attributes:
+        source:     The source URL of the gem.
+        checksum:   The checksum of the gem.
+    """
+
+    source: str
+    checksum: Optional[str] = None
+
+
+class GitDependency(_GemMetadata):
+    """
+    Represents a git dependency.
+
+    Attributes:
+        url:        The URL of the git repository.
+        ref:        Commit hash.
+    """
+
+    url: AcceptedUrl
+    ref: AcceptedGitRef
+
+
+class PathDependency(_GemMetadata):
+    """
+    Represents a path dependency.
+
+    Attributes:
+        path:       Subpath from package root.
+    """
+
+    path: str
+
+
+BundlerDependency = Union[GemDependency, GitDependency, PathDependency]
+ParseResult = list[BundlerDependency]
+
+
+def parse_lockfile(package_dir: RootedPath) -> ParseResult:
+    """Parse a Gemfile.lock file and return a list of dependencies."""
+    lockfile_path = package_dir.join_within_root(GEMFILE_LOCK)
+    gemfile_path = package_dir.join_within_root(GEMFILE)
+    if not lockfile_path.path.exists() or not gemfile_path.path.exists():
+        reason = "Gemfile and Gemfile.lock must be present in the package directory"
+        solution = (
+            "Run `bundle init` to generate the Gemfile.\n"
+            "Run `bundle lock` to generate the Gemfile.lock."
+        )
+        raise PackageRejected(reason=reason, solution=solution)
+
+    scripts_dir = Path(__file__).parent / "scripts"
+    lockfile_parser = scripts_dir / "lockfile_parser.rb"
+    try:
+        output = run_cmd(cmd=[str(lockfile_parser)], params={"cwd": package_dir.path})
+    except subprocess.CalledProcessError:
+        raise PackageManagerError(f"Failed to parse {lockfile_path}")
+
+    json_output = json.loads(output)
+
+    bundler_version: str = json_output["bundler_version"]
+    log.info("Package %s is bundled with version %s", package_dir.path.name, bundler_version)
+    dependencies: list[dict[str, str]] = json_output["dependencies"]
+
+    result: ParseResult = []
+    for dep in dependencies:
+        if dep["type"] == "rubygems":
+            result.append(GemDependency(**dep))
+        elif dep["type"] == "git":
+            result.append(GitDependency(**dep))
+        elif dep["type"] == "path":
+            _validate_path_dependency_subpath(package_dir, dep["path"])
+            result.append(PathDependency(**dep))
+
+    return result
+
+
+def _validate_path_dependency_subpath(package_dir: RootedPath, path: str) -> None:
+    """Validate that the path dependency is within the package root."""
+    try:
+        package_dir.join_within_root(path)
+    except PathOutsideRoot:
+        reason = "PATH dependencies should be within the package root"
+        raise UnexpectedFormat(reason=reason)
