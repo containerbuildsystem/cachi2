ac0eae9f1df14a79dc5f2907687eec2c08222d2f
Michal Šoltis <msoltis@redhat.com>
2024-09-04 10:38:40 +0200

    bundler: Implement parsing "main" package name and version
    Main package is a package that is being processed
    by cachi2 at given time. It is part of generated SBOM.
    Therefore it must have a name and optionally a version.
    
    In the world of rubygems / bundler a file that holds
    data about a package is called "gemspec". It holds
    other fields as well, such as description, authors,
    license, etc.
    
    It is not a common scenario, developing a gem
    and having `Gemfile.lock` at the same time,
    but it is possible. When the file is explicitely
    imported in `Gemfile`, Bundler will create a record
    inside `Gemfile.lock` that represents that gem
    as a PATH dependency with subpath (remote) set to "."
    
    If the file does not exists, we fall back to extract
    name from git URL and the version is None in that
    case. This approach is used in pip module as well
    and it might be used more often.
    
    Signed-off-by: Michal Šoltis <msoltis@redhat.com>

--- cachi2/core/package_managers/bundler/main.py
+++ cachi2/core/package_managers/bundler/main.py
@@ -1,8 +1,16 @@
+import logging
+from pathlib import Path
+from typing import Optional
+
+from cachi2.core.errors import PackageRejected, UnsupportedFeature
 from cachi2.core.models.input import Request
 from cachi2.core.models.output import EnvironmentVariable, ProjectFile, RequestOutput
 from cachi2.core.models.sbom import Component
-from cachi2.core.package_managers.bundler.parser import parse_lockfile
+from cachi2.core.package_managers.bundler.parser import ParseResult, PathDependency, parse_lockfile
 from cachi2.core.rooted_path import RootedPath
+from cachi2.core.scm import get_repo_id
+
+log = logging.getLogger(__name__)
 
 
 def fetch_bundler_source(request: Request) -> RequestOutput:
@@ -24,5 +32,70 @@ def fetch_bundler_source(request: Request) -> RequestOutput:
 
 def _resolve_bundler_package(package_dir: RootedPath, output_dir: RootedPath) -> list[Component]:
     """Process a request for a single bundler package."""
-    parse_lockfile(package_dir)
+    dependencies = parse_lockfile(package_dir)
+    _get_main_package_name_and_version(package_dir, dependencies)
     return []
+
+
+def _get_main_package_name_and_version(
+    package_dir: RootedPath,
+    dependencies: ParseResult,
+) -> tuple[str, Optional[str]]:
+    """
+    Get main package name and version.
+
+    The main package is the package that is being processed by cachi2.
+    Not any of its dependencies.
+    """
+    name_and_version = _get_name_and_version_from_lockfile(dependencies)
+    if name_and_version is not None:
+        return name_and_version
+
+    log.info("Failed to extract name and version from gemspec file")
+
+    # fallback to origin remote
+    try:
+        name = _get_repo_name_from_origin_remote(package_dir)
+    # if the git repository does not have an origin remote
+    except UnsupportedFeature:
+        raise PackageRejected(
+            reason="Failed to extract package name from origin remote",
+            solution=(
+                "Please specify package name and version in a way that Cachi2 understands,\n"
+                "or make sure that the directory Cachi2 is processing is a git repository with\n"
+                "an 'origin' remote, in which case Cachi2 will infer the package name from the remote URL."
+            ),
+        )
+
+    return name, None
+
+
+def _get_name_and_version_from_lockfile(dependencies: ParseResult) -> Optional[tuple[str, str]]:
+    """
+    Extract the package name and version from dependencies in the Gemfile.lock.
+
+    Gemfile.lock only contains the name and version of the package. If the gemspec file
+    is explicitly defined in the Gemfile, Bundler will create a path dependency record
+    representing the gem in the package directory.
+
+    Note that having a gemspec file is an edge case when the package is not an actual gem.
+    But it is possible to include a gemspec file in the package directory that defines its name,
+    version, and other metadata even if the package is not a gem. So we respect this edge case.
+
+    See design doc for more details:
+    https://github.com/containerbuildsystem/cachi2/blob/main/docs/design/bundler.md
+    """
+    for dep in dependencies:
+        if isinstance(dep, PathDependency) and dep.path == ".":
+            return dep.name, dep.version
+
+    return None
+
+
+def _get_repo_name_from_origin_remote(package_dir: RootedPath) -> str:
+    """Extract repository name from git origin remote in the package directory."""
+    repo_path = get_repo_id(package_dir.root).parsed_origin_url.path
+    repo_name = Path(repo_path).stem
+
+    resolved_path = Path(repo_name).joinpath(package_dir.subpath_from_root)
+    return str(resolved_path)
---
+++ tests/unit/package_managers/bundler/test_main.py
@@ -0,0 +1,55 @@
+import pytest
+from git.repo import Repo
+
+from cachi2.core.errors import PackageRejected
+from cachi2.core.package_managers.bundler.main import _get_main_package_name_and_version
+from cachi2.core.package_managers.bundler.parser import GemDependency, ParseResult, PathDependency
+from cachi2.core.rooted_path import RootedPath
+
+
+def test_get_main_package_name_and_version(rooted_tmp_path: RootedPath) -> None:
+    dependencies: ParseResult = [
+        GemDependency(
+            name="my_gem_dep",
+            version="0.1.0",
+            source="https://rubygems.org",
+        ),
+        PathDependency(
+            name="my_path_dep",
+            version="0.2.0",
+            path=".",
+        ),
+    ]
+
+    name, version = _get_main_package_name_and_version(
+        package_dir=rooted_tmp_path, dependencies=dependencies
+    )
+    assert name == "my_path_dep"
+    assert version == "0.2.0"
+
+
+def test_get_main_package_name_and_version_from_repo(
+    rooted_tmp_path_repo: RootedPath,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    repo = Repo(rooted_tmp_path_repo)
+    repo.create_remote("origin", "git@github.com:user/example.git")
+
+    name, version = _get_main_package_name_and_version(
+        package_dir=rooted_tmp_path_repo, dependencies=[]
+    )
+    assert "Failed to extract name and version from gemspec file" in caplog.messages
+
+    assert name == "example"
+    assert version is None
+
+
+def test_get_main_package_name_and_version_from_repo_without_origin(
+    rooted_tmp_path_repo: RootedPath,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    with pytest.raises(PackageRejected) as exc_info:
+        _get_main_package_name_and_version(package_dir=rooted_tmp_path_repo, dependencies=[])
+
+    assert "Failed to extract name and version from gemspec file" in caplog.messages
+    assert "Failed to extract package name from origin remote" in exc_info.value.friendly_msg()
