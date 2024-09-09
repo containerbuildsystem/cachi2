037540f7cbffe8b021c94f812b272325d30d074b
Michal Šoltis <msoltis@redhat.com>
2024-09-09 14:09:27 +0200

    Merge two conftest.py files to only one
    Both files have only a couple of lines of code.
    Let's merge them together - move fixtures from
    `tests.unit.package_managers.conftest.py` to
    `tests.unit.conftest.py`.
    
    Signed-off-by: Michal Šoltis <msoltis@redhat.com>

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,7 +1,10 @@
 import tarfile
 from pathlib import Path
 
+import git
 import pytest
+
+from cachi2.core.rooted_path import RootedPath
 
 
 @pytest.fixture
@@ -17,3 +20,21 @@ def golang_repo_path(data_dir: Path, tmp_path: Path) -> Path:
         tar.extractall(tmp_path)
 
     return tmp_path / "golang_git_repo"
+
+
+@pytest.fixture()
+def rooted_tmp_path(tmp_path: Path) -> RootedPath:
+    return RootedPath(tmp_path)
+
+
+@pytest.fixture()
+def rooted_tmp_path_repo(rooted_tmp_path: RootedPath) -> RootedPath:
+    repo = git.Repo.init(rooted_tmp_path)
+    repo.git.config("user.name", "user")
+    repo.git.config("user.email", "user@example.com")
+
+    Path(rooted_tmp_path, "README.md").touch()
+    repo.index.add(["README.md"])
+    repo.index.commit("Initial commit")
+
+    return rooted_tmp_path
