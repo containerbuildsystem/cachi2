bc14fa06337bf707ae5b80807d3f3e89d1e7b051
Michal Šoltis <msoltis@redhat.com>
2024-09-09 14:44:00 +0200

    Update docstring for pytest fixtures
    Signed-off-by: Michal Šoltis <msoltis@redhat.com>

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -9,26 +9,28 @@ from cachi2.core.rooted_path import RootedPath
 
 @pytest.fixture
 def data_dir() -> Path:
-    """Path to the directory for storing unit test data."""
+    """Return Path object for the directory that stores unit test data."""
     return Path(__file__).parent / "data"
 
 
 @pytest.fixture
 def golang_repo_path(data_dir: Path, tmp_path: Path) -> Path:
-    """Extract the golang git repo tarball to a tmpdir, return the path to the repo."""
+    """Return extracted Golang git repository inside a temporary directory."""
     with tarfile.open(data_dir / "golang_git_repo.tar.gz") as tar:
         tar.extractall(tmp_path)
 
     return tmp_path / "golang_git_repo"
 
 
-@pytest.fixture()
+@pytest.fixture
 def rooted_tmp_path(tmp_path: Path) -> RootedPath:
+    """Return RootedPath object wrapper for the tmp_path fixture."""
     return RootedPath(tmp_path)
 
 
-@pytest.fixture()
+@pytest.fixture
 def rooted_tmp_path_repo(rooted_tmp_path: RootedPath) -> RootedPath:
+    """Return RootedPath object wrapper for the tmp_path fixture with initialized git repository."""
     repo = git.Repo.init(rooted_tmp_path)
     repo.git.config("user.name", "user")
     repo.git.config("user.email", "user@example.com")
