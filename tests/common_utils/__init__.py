4e0d3e922167cc9479a3b0508fb917e0cf71ec77
Michal Šoltis <msoltis@redhat.com>
2024-09-16 12:21:51 +0200

    Drop redundant assert_directories_equal function
    The function is not used anywhere in the tests.
    
    Signed-off-by: Michal Šoltis <msoltis@redhat.com>

--- tests/common_utils/__init__.py
+++ tests/common_utils/__init__.py
@@ -1,36 +1,8 @@
-import filecmp
 import os
 from pathlib import Path
 from typing import Union
 
 GIT_REF = "f" * 40
-
-
-def assert_directories_equal(dir_a: str, dir_b: str) -> None:
-    """
-    Check recursively directories have equal content.
-
-    :param dir_a: first directory to check
-    :param dir_b: second directory to check
-    """
-    dirs_cmp = filecmp.dircmp(dir_a, dir_b)
-    assert (
-        len(dirs_cmp.left_only) == 0
-    ), f"Found files: {dirs_cmp.left_only} in {dir_a}, but not {dir_b}."
-    assert (
-        len(dirs_cmp.right_only) == 0
-    ), f"Found files: {dirs_cmp.right_only} in {dir_b}, but not {dir_a}."
-    assert (
-        len(dirs_cmp.funny_files) == 0
-    ), f"Found files: {dirs_cmp.funny_files} in {dir_a}, {dir_b} which could not be compared."
-    (_, mismatch, errors) = filecmp.cmpfiles(dir_a, dir_b, dirs_cmp.common_files, shallow=False)
-    assert len(mismatch) == 0, f"Found mismatches: {mismatch} between {dir_a} {dir_b}."
-    assert len(errors) == 0, f"Found errors: {errors} between {dir_a} {dir_b}."
-
-    for common_dir in dirs_cmp.common_dirs:
-        inner_a = os.path.join(dir_a, common_dir)
-        inner_b = os.path.join(dir_b, common_dir)
-        assert_directories_equal(inner_a, inner_b)
 
 
 class Symlink(str):
