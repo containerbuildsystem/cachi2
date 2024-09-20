8920cb320a5976e49dca87925c2b7d4f22696a67
Michal Šoltis <msoltis@redhat.com>
2024-09-20 07:34:39 +0200

    bundler: Validate PATH dependencies inside of the pydantic model
    To do so, it is necessary to add a new attribute
    `root` to the Path Dependency class. With this
    change, it is possible to move subpath validation
    function to the class as pydantic validator.
    
    Signed-off-by: Michal Šoltis <msoltis@redhat.com>

--- tests/unit/package_managers/bundler/test_main.py
+++ tests/unit/package_managers/bundler/test_main.py
@@ -17,6 +17,7 @@ def test_get_main_package_name_and_version(rooted_tmp_path: RootedPath) -> None:
         PathDependency(
             name="my_path_dep",
             version="0.2.0",
+            root=str(rooted_tmp_path),
             subpath=".",
         ),
     ]
