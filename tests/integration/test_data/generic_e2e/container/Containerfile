25704d7aedbb50237e85f9bfc7c3b49adb625cfa
Jan Koscielniak <jakoscie@redhat.com>
2024-10-16 18:02:40 +0200

    generic fetcher: Add integration tests
    Implements integration test suite for the generic fetcher.
    
    Signed-off-by: Jan Koscielniak <jakoscie@redhat.com>

---
+++ tests/integration/test_data/generic_e2e/container/Containerfile
@@ -0,0 +1,5 @@
+FROM registry.fedoraproject.org/fedora:40
+
+RUN cp -r /tmp/generic_e2e-output/deps/generic/ /deps
+
+CMD ["ls", "-al", "/deps"]
---
+++ tests/integration/test_generic.py
@@ -0,0 +1,110 @@
+from pathlib import Path
+from typing import List
+
+import pytest
+
+from . import utils
+
+
+@pytest.mark.parametrize(
+    "test_params",
+    [
+        pytest.param(
+            utils.TestParameters(
+                repo="https://github.com/cachito-testing/cachi2-generic",
+                ref="test-file-not-reachable",
+                packages=({"path": ".", "type": "generic"},),
+                flags=["--dev-package-managers"],
+                check_output=False,
+                check_deps_checksums=False,
+                check_vendor_checksums=False,
+                expected_exit_code=1,
+                expected_output="Unsuccessful download",
+            ),
+            id="generic_file_not_reachable",
+        )
+    ],
+)
+def test_generic_fetcher(
+    test_params: utils.TestParameters,
+    cachi2_image: utils.ContainerImage,
+    tmp_path: Path,
+    test_data_dir: Path,
+    request: pytest.FixtureRequest,
+) -> None:
+    """
+    Test fetched dependencies for the generic fetcher.
+
+    :param test_params: Test case arguments
+    :param tmp_path: Temp directory for pytest
+    """
+    test_case = request.node.callspec.id
+
+    source_folder = utils.clone_repository(
+        test_params.repo, test_params.ref, f"{test_case}-source", tmp_path
+    )
+
+    utils.fetch_deps_and_check_output(
+        tmp_path,
+        test_case,
+        test_params,
+        source_folder,
+        test_data_dir,
+        cachi2_image,
+    )
+
+
+@pytest.mark.parametrize(
+    "test_params,check_cmd,expected_cmd_output",
+    [
+        pytest.param(
+            utils.TestParameters(
+                repo="https://github.com/cachito-testing/cachi2-generic",
+                ref="test-e2e",
+                packages=({"path": ".", "type": "generic"},),
+                flags=["--dev-package-managers"],
+                check_output=True,
+                check_deps_checksums=True,
+                check_vendor_checksums=False,
+                expected_exit_code=0,
+            ),
+            ["ls", "/deps"],
+            ["archive.zip\nv1.0.0.zip\n"],
+            id="generic_e2e",
+        ),
+    ],
+)
+def test_e2e_generic(
+    test_params: utils.TestParameters,
+    check_cmd: List[str],
+    expected_cmd_output: str,
+    cachi2_image: utils.ContainerImage,
+    tmp_path: Path,
+    test_data_dir: Path,
+    request: pytest.FixtureRequest,
+) -> None:
+    """
+    End to end test for generic fetcher.
+
+    :param test_params: Test case arguments
+    :param tmp_path: Temp directory for pytest
+    """
+    test_case = request.node.callspec.id
+
+    source_folder = utils.clone_repository(
+        test_params.repo, test_params.ref, f"{test_case}-source", tmp_path
+    )
+
+    output_folder = utils.fetch_deps_and_check_output(
+        tmp_path, test_case, test_params, source_folder, test_data_dir, cachi2_image
+    )
+
+    utils.build_image_and_check_cmd(
+        tmp_path,
+        output_folder,
+        test_data_dir,
+        test_case,
+        check_cmd,
+        expected_cmd_output,
+        cachi2_image,
+    )
