27b495b16d3cdb6cd0bee3faf3b10a00fbd43e9c
Alexey Ovchinnikov <aovchinn@redhat.com>
2024-10-24 07:43:52 -0500

    yarn: Introduce new VersionRange handler class
    This change introduces VersionRange class to simplify
    checks for version belonging to a range of versions.
    
    Signed-off-by: Alexey Ovchinnikov <aovchinn@redhat.com>

--- cachi2/core/package_managers/yarn/utils.py
+++ cachi2/core/package_managers/yarn/utils.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-from typing import Optional
+from typing import Optional, Union
 
 from semver import Version
 
@@ -29,6 +29,60 @@ def run_yarn_cmd(
     except subprocess.CalledProcessError as e:
         # the yarn command writes the errors to stdout
         raise PackageManagerError(f"Yarn command failed: {' '.join(cmd)}", stderr=e.stdout)
+
+
+SemverLike = Union[Version, str]
+
+
+class VersionsRange:
+    """Represents a version range for cleaner version constrains checks.
+
+    Versions range is a right-open interval:
+    >>> Version.parse("1.2.3") in VersionsRange("3.0.0", "4.0.0")
+    False
+    >>> Version.parse("1.2.3") in VersionsRange("1.0.0", "2.0.0")
+    True
+    >>> Version.parse("1.0.0") in VersionsRange("1.0.0", "2.0.0")
+    True
+    >>> Version.parse("2.0.0") in VersionsRange("1.0.0", "2.0.0")
+    False
+
+    Release candidates are a special case, they are ignored within the
+    interval and cause immediate rejection on any of the boundaries:
+    >>> Version.parse("2.0.0-rc1") in VersionsRange("1.0.0", "2.0.0")
+    False
+    >>> Version.parse("1.0.0-rc1") in VersionsRange("1.0.0", "2.0.0")
+    False
+    >>> Version.parse("1.5.0-rc1") in VersionsRange("1.0.0", "2.0.0")
+    True
+    """
+
+    def __init__(self, min_ver: SemverLike, max_ver: SemverLike) -> None:
+        """Initialize a version range."""
+        self.min_ver = min_ver if isinstance(min_ver, Version) else Version.parse(min_ver)
+        self.max_ver = max_ver if isinstance(max_ver, Version) else Version.parse(max_ver)
+
+    def __contains__(self, other: Version) -> bool:
+        if not isinstance(other, self.min_ver.__class__):
+            return False
+        # The original version check logic (as captured in UTs) broke with direct
+        # version comparison rules, e.g. 4.0.0-rc1 would have been considered
+        # version 4 and would  have been rejected basing on major version
+        # only. Direct comparison of Version object would have resulted in 4.0.0-rc1
+        # being strictly within 4.0.0 interval (since rcN is not a version yet).
+        # The original implementation considered anything starting with
+        # a different major version an outlier. The logic below captures this
+        # with a special case for versions with prerelease field set.
+        if other.prerelease:
+            # Drop prerelease:
+            other_ver = Version(other.major, other.minor, other.patch)
+            # Treat boundaries separately:
+            if other_ver == self.min_ver or other_ver == self.max_ver:
+                return False
+            # Continue as usual otherwise:
+            return other_ver >= self.min_ver and other < self.max_ver
+        else:
+            return other >= self.min_ver and other < self.max_ver
 
 
 def extract_yarn_version_from_env(source_dir: RootedPath, env: Optional[dict] = None) -> Version:
