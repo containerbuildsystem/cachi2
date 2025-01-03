7557f2c6ab01523de5a544e2ab34e4134b976069
Bruno Pimentel <bpimente@redhat.com>
2025-01-03 17:11:35 -0300

    yarn: remove unused registry_server_for_scope function
    There's no need for Cachi2 to directly access the contents of the
    "npmRegistryServer" yarn_rc variable, since Yarn itself will read it
    when we execute "yarn install". The code refering to it is a leftover of
    the development phase.
    
    Signed-off-by: Bruno Pimentel <bpimente@redhat.com>

--- cachi2/core/package_managers/yarn/project.py
+++ cachi2/core/package_managers/yarn/project.py
@@ -49,17 +49,6 @@ class YarnRc(UserDict):
         self._path = path
         super().__init__(data)
 
-    def registry_server_for_scope(self, scope: str) -> str:
-        """Get the configured registry server for a scoped package.
-
-        Fallback to the global defined registry server if there's no configuration for this specific
-        scope.
-
-        See: https://v3.yarnpkg.com/configuration/yarnrc#npmScopes
-        """
-        registry = self.data.get("npmScopes", {}).get(scope, {}).get("npmRegistryServer")
-        return registry or self["npmRegistryServer"]
-
     def write(self) -> None:
         """Write the data to the yarnrc file."""
         with self._path.path.open("w") as f:
